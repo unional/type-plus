15327d7a11b02d1141053770d3719bb9cd6c7093
Homa Wong (unional) <homawong@gmail.com>
2018-12-28 02:54:16 -0800

    fix: return type of find and map

--- src/mapKey.ts
+++ src/mapKey.ts
@@ -1,3 +1,3 @@
-export function mapKey<R = {}, T = {}>(subject: T, callbackfn: (key: keyof T, index: number, obj: string[]) => R, thisArg?: any) {
+export function mapKey<R, T>(subject: T, callbackfn: (key: keyof T, index: number, obj: string[]) => R, thisArg?: any): R[] {
   return Object.keys(subject).map(callbackfn as any, thisArg)
 }
