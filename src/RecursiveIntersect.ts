0616cfd290276c0a0c4248337cc276f320a3a3c9
Homa Wong (unional) <homawong@gmail.com>
2019-01-01 16:43:21 -0800

    fix: improve RecursiveIntersect for array property.

--- src/RecursiveIntersect.ts
+++ src/RecursiveIntersect.ts
@@ -1,12 +1,16 @@
 /**
  * Intersect type recursively.
  * The recursion terminates at level 7 due to design limit of TypeScript.
+ *
+ * Normal use case is intersecting betwee two object types.
+ * While it works for value types and top level array,
+ * top level array does not recursive into the elements.
  */
 export type RecursiveIntersect<T, U> = (T & (
   T extends Array<infer Y> ? Array<Y & U> & U :
   T extends object ? {
     [P in keyof T]:
-    T[P] extends Array<infer R> ? RecursiveIntersect<R, U>[] :
+    T[P] extends Array<infer R> ? Array<RecursiveIntersect<R, U>> & U :
     T[P] extends object ? RecursiveIntersect<T[P], U> :
     T[P] & U
   } & U : U)
