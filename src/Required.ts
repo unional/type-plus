dc69a5168dbdbcc90b238380683d8daef2a02b4b
Homa Wong (unional) <homawong@gmail.com>
2019-01-04 16:44:23 -0800

    fix: improve Partial* and Required*

--- src/Required.ts
+++ src/Required.ts
@@ -1,9 +1,5 @@
 // Thanks [jack-williams](https://github.com/jack-williams) for the [solution](https://github.com/Microsoft/TypeScript/issues/29269#issuecomment-451602962)
 
-export type RequiredPick<T, U extends keyof T> = {
-  [P in U]-?: Exclude<T[P], undefined>
-} & Pick<T, Exclude<keyof T, U>>
+export type RequiredPick<T, U extends keyof T> = Required<Pick<T, U>> & Pick<T, Exclude<keyof T, U>>
 
-export type RequiredExcept<T, U extends keyof T> = {
-  [P in Exclude<keyof T, U>]-?: Exclude<T[P], undefined>
-} & Pick<T, U>
+export type RequiredExcept<T, U extends keyof T> = Required<Pick<T, Exclude<keyof T, U>>> & Pick<T, U>
