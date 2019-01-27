e843acb42fce19e23dd2c312e6c7461c75299681
Homa Wong (unional) <homawong@gmail.com>
2019-01-27 00:19:37 -0800

    test: improve test

--- src/literalArray.spec.ts
+++ src/literalArray.spec.ts
@@ -4,8 +4,8 @@ import { isNever } from './test-util';
 test('entries in array are restricted to the input literals', () => {
   const actual = literalArray('a', 'b')
 
-  let y = actual[0]
   // the cast is just for assignment. Does not affect type.
-  let x: (typeof y) & 'c' = 1 as never
+  let x: (typeof actual[number]) & 'c' = 'c' as never
+
   isNever(x)
 })
