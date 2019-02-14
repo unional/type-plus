50623812d06a9647d138879c8531111c0d74a18c
Homa Wong (unional) <homawong@gmail.com>
2019-02-14 14:08:16 -0800

    test: add test on key type

--- src/reduceKey.spec.ts
+++ src/reduceKey.spec.ts
@@ -6,3 +6,12 @@ test('predicate key can be used as indexer of the subject', () => {
   const actual = reduceKey(subject, (p, k) => p += subject[k], 'a')
   t.strictEqual(actual, 'a123')
 })
+
+// currently k: never but it somehow works.
+// Can't find a way to get k to be a more reasonable type that works with `subject[k]`
+// So leaving it as is for now.
+test('key type is string if subject type is plain object', () => {
+  const subject: {} = { a: 1 }
+  const actual = reduceKey(subject, (p, k) => p += subject[k], 'a')
+  t.strictEqual(actual, 'a1')
+})
--- src/reduceKey.ts
+++ src/reduceKey.ts
@@ -1,4 +1,3 @@
 export function reduceKey<T, S>(subject: S, callbackfn: (previousValue: T, key: keyof S, currentIndex: number, array: string[]) => T, initialValue: T): T {
   return Object.keys(subject).reduce(callbackfn as any, initialValue)
 }
-
