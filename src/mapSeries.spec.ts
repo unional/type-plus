c96afc50051193cfd9339e14ff10687fd044bfb9
Homa Wong (unional) <homawong@gmail.com>
2019-03-17 02:12:47 -0700

    fix test

--- src/mapSeries.spec.ts
+++ src/mapSeries.spec.ts
@@ -1,11 +1,12 @@
 import { mapSeries } from '.';
 
-test('map over values', async () => {
+test('map over values', () => {
   const values = [3, 2, 1]
   let actual = ''
 
-  const result = await mapSeries(values, v => new Promise<string>(a => setTimeout(() => a(actual += v), v * 10)))
-
-  expect(actual).toBe('321')
-  expect(result).toEqual(['3', '32', '321'])
+  return mapSeries(values, v => new Promise<string>(a => setTimeout(() => a(actual += v), v * 10)))
+    .then(result => {
+      expect(actual).toBe('321')
+      expect(result).toEqual(['3', '32', '321'])
+    })
 })
