b49054bc2f5d49904217e144ec3bca8318c8ea07
Homa Wong (unional) <homawong@gmail.com>
2019-03-17 02:05:16 -0700

    fix: use Promise to keep NodeJS6 support

--- src/mapSeries.ts
+++ src/mapSeries.ts
@@ -1,7 +1,9 @@
-export async function mapSeries<R, T = any>(values: T[], fn: (value: T) => Promise<R>): Promise<R[]> {
+export function mapSeries<R, T = any>(values: T[], fn: (value: T) => Promise<R>): Promise<R[]> {
   const results: R[] = []
-  for (let i = 0; i < values.length; i++) {
-    results.push(await fn(values[i]))
-  }
-  return results
+  return values.reduce((p, v) => {
+    return p.then(() => fn(v)).then((r: any) => {
+      results.push(r)
+      return results
+    })
+  }, Promise.resolve() as any)
 }
