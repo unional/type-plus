abc8980ac85c0d0a83b3b700ac5cdd26f655dd26
Homa Wong (unional) <homawong@gmail.com>
2019-06-13 19:25:10 -0700

    fix: add Omit, deprecate Except

--- src/Except.spec.ts
+++ src/Except.spec.ts
@@ -7,11 +7,13 @@ test('Remove properties', () => {
     c: boolean
   }
 
+  // tslint:disable-next-line: deprecation
   type Actual = Except<Foo, 'c'>
   let a: Actual = {} as any
   typeAssert.isNumber(a.a)
   typeAssert.isString(a.b)
 
+  // tslint:disable-next-line: deprecation
   type Revert = Except<Foo, keyof Actual>
   let r: Revert = {} as any
   typeAssert.isBoolean(r.c)
