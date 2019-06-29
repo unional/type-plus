b225c244aad4c45706ec2966684a7301d423ea8a
Homa Wong (unional) <homawong@gmail.com>
2019-06-28 21:08:04 -0700

    update readme and deprecate PartialExcept

--- src/Partial.ts
+++ src/Partial.ts
@@ -3,6 +3,9 @@ import { UnionKeys } from './UnionKeys';
 
 export type PartialPick<T, U extends UnionKeys<T>> = T extends T ? Pick<T, Exclude<keyof T, U>> & Partial<Pick<T, U>> : never
 
+/**
+ * @deprecated replaced by `PartialOmit`
+ */
 export type PartialExcept<T, U extends UnionKeys<T>> = T extends T ? Pick<T, U> & Partial<Pick<T, Exclude<keyof T, U>>> : never
 
 export type PartialOmit<T, U extends UnionKeys<T>> = T extends T ? Pick<T, U> & Partial<Pick<T, Exclude<keyof T, U>>> : never
