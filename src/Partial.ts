dbdc66fc61fc2ba2154cfdd9b4b34769b85840ad
Homa Wong (unional) <homawong@gmail.com>
2019-06-28 21:04:16 -0700

    feat: add PartialOmit

--- src/Partial.ts
+++ src/Partial.ts
@@ -1,3 +1,8 @@
-export type PartialPick<T, U extends keyof T> = Pick<T, Exclude<keyof T, U>> & Partial<Pick<T, U>>
+import { Pick } from './pick';
+import { UnionKeys } from './UnionKeys';
 
-export type PartialExcept<T, U extends keyof T> = Pick<T, U> & Partial<Pick<T, Exclude<keyof T, U>>>
+export type PartialPick<T, U extends UnionKeys<T>> = T extends T ? Pick<T, Exclude<keyof T, U>> & Partial<Pick<T, U>> : never
+
+export type PartialExcept<T, U extends UnionKeys<T>> = T extends T ? Pick<T, U> & Partial<Pick<T, Exclude<keyof T, U>>> : never
+
+export type PartialOmit<T, U extends UnionKeys<T>> = T extends T ? Pick<T, U> & Partial<Pick<T, Exclude<keyof T, U>>> : never
