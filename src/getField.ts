5600aabc4a3b831c45a858b51988082bbd03d314
Homa Wong (unional) <homawong@gmail.com>
2019-07-06 15:41:47 -0700

    fix: getField() to support union types

--- src/getField.ts
+++ src/getField.ts
@@ -1,18 +1,20 @@
+import { UnionKeys } from './UnionKeys';
+
 export function getField<
   T,
   TX extends Exclude<T, undefined | null>,
-  K extends keyof TX,
+  K extends UnionKeys<TX>,
   >(subject: T, key: K): TX[K]
 export function getField<
   T,
   TX extends Exclude<T, undefined | null>,
-  K extends keyof TX,
+  K extends UnionKeys<TX>,
   DV extends Exclude<TX[K], undefined>
 >(subject: T, key: K, defaultValue: DV): DV
 export function getField<
   T,
   TX extends Exclude<T, undefined | null>,
-  K extends keyof TX,
+  K extends UnionKeys<TX>,
   DV extends Exclude<TX[K], undefined>
 >(subject: T, key: K, defaultValue?: DV): DV {
   return subject && (subject as any)[key] || defaultValue
