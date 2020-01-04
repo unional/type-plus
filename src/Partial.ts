e1afb939827e2e17865b1d615277fbe9210cf290
Homa Wong <homawong@gmail.com>
2020-01-04 15:20:32 -0800

    fix: improve PartialPick and PartialOmit (#49)
    this works better with nested generics

--- src/Partial.ts
+++ src/Partial.ts
@@ -1,11 +1,18 @@
-import { Pick } from './pick';
-import { UnionKeys } from './UnionKeys';
+import { Omit } from './Omit'
+import { Pick } from './pick'
+import { UnionKeys } from './UnionKeys'
 
-export type PartialPick<T, U extends UnionKeys<T>> = T extends T ? Pick<T, Exclude<keyof T, U>> & Partial<Pick<T, U>> : never
+/**
+ * Apply `Partial<>` on the selected properties.
+ */
+export type PartialPick<T, U extends UnionKeys<T>> = T extends T ? Omit<T, U> & Partial<Pick<T, U>> : never
 
 /**
  * @deprecated replaced by `PartialOmit`
  */
-export type PartialExcept<T, U extends UnionKeys<T>> = T extends T ? Pick<T, U> & Partial<Pick<T, Exclude<keyof T, U>>> : never
+export type PartialExcept<T, U extends UnionKeys<T>> = T extends T ? Pick<T, U> & Partial<Omit<T, U>> : never
 
-export type PartialOmit<T, U extends UnionKeys<T>> = T extends T ? Pick<T, U> & Partial<Pick<T, Exclude<keyof T, U>>> : never
+/**
+ * Apply `Partial<>` on all not selected properties.
+ */
+export type PartialOmit<T, U extends UnionKeys<T>> = T extends T ? Pick<T, U> & Partial<Omit<T, U>> : never
