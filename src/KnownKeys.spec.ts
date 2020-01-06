376f36fec946bfa4352d9121d480bc8ca10a3e59
Homa Wong <homawong@gmail.com>
2020-01-05 21:59:22 -0800

    fix: primitive and empty object

--- src/KnownKeys.spec.ts
+++ src/KnownKeys.spec.ts
@@ -7,6 +7,33 @@ test('pick out only known keys', () => {
     [k: string]: any,
   }
 
-  const x: KnownKeys<A> = {} as any
-  assertType<'a' | 'b'>(x)
+  const input: A = {}
+  const actual = getKnownKeys(input)
+  assertType<'a' | 'b'>(actual)
 })
+
+test('primitive type yields never', () => {
+  assertType.isNever(getKnownKeys(undefined))
+  assertType.isNever(getKnownKeys(true))
+  assertType.isNever(getKnownKeys(false))
+  assertType.isNever(getKnownKeys(null))
+  assertType.isNever(getKnownKeys('str'))
+  assertType.isNever(getKnownKeys(1))
+  assertType.isNever(getKnownKeys({}))
+  assertType.isNever(getKnownKeys([]))
+})
+
+test('literal gets keys', () => {
+  assertType<'a'>(getKnownKeys({ a: 1 }))
+})
+
+test('empty record yields never', () => {
+  const x: Record<any, any> = {}
+  const actual = getKnownKeys(x)
+  assertType.isNever(actual)
+})
+
+
+function getKnownKeys<T>(_value: T): KnownKeys<T> {
+  return {} as any
+}
