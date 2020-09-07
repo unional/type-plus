edd10c83f1a49c4c9f74e6df88ae283235cb8cf5
unional <homawong@gmail.com>
2020-09-07 16:28:35 -0700

    chore: update devDeps

---
+++ babel.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+    ['@babel/preset-typescript', {
+      allowNamespaces: true
+    }],
+  ],
+  plugins: [
+    '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-proposal-nullish-coalescing-operator'
+  ],
+};
