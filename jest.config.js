d9b8cea9eebf3e85feba4a5a10bd53fb8788ca1d
unional <homawong@gmail.com>
2020-09-07 16:28:35 -0700

    chore: update devDeps

--- jest.config.js
+++ jest.config.js
@@ -1,1 +1,48 @@
-module.exports = require('@unional/devpkg-node/simple/config/jest.common')
+const isCI = require('is-ci')
+module.exports = isCI ? {
+  'collectCoverageFrom': [
+    '<rootDir>/src/**/*.[jt]s',
+    '!<rootDir>/src/bin.[jt]s',
+  ],
+  'roots': [
+    '<rootDir>/src',
+  ],
+  'reporters': [
+    'default',
+    [
+      'jest-junit',
+      {
+        'output': '.reports/junit/js-test-results.xml',
+      },
+    ],
+  ],
+  'testEnvironment': 'node',
+  'testMatch': ['**/?(*.)+(spec|test|integrate|accept|system|unit).[jt]s?(x)'],
+} : {
+    'collectCoverageFrom': [
+      '<rootDir>/src/**/*.[jt]s',
+      '!<rootDir>/src/bin.[jt]s',
+    ],
+    'reporters': [
+      'default',
+      'jest-progress-tracker',
+      ['jest-audio-reporter', { volume: 0.3 }],
+    ],
+    'roots': [
+      '<rootDir>/src',
+    ],
+    'testEnvironment': 'node',
+    'testMatch': ['**/?(*.)+(spec|test|integrate|accept|system|unit).[jt]s?(x)'],
+    'watchPlugins': [
+      'jest-watch-suspend',
+      'jest-watch-repeat',
+      'jest-watch-typeahead/filename',
+      'jest-watch-typeahead/testname',
+      [
+        'jest-watch-toggle-config', { 'setting': 'verbose' },
+      ],
+      [
+        'jest-watch-toggle-config', { 'setting': 'collectCoverage' },
+      ],
+    ],
+  }
