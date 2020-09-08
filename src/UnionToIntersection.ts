adbbfebf2cb5630308c79c8635918dbccaecf2dc
unional <homawong@gmail.com>
2020-09-07 17:59:10 -0700

    test: ignore coverage for toIntersection
    It is not yet exposed

--- src/UnionToIntersection.ts
+++ src/UnionToIntersection.ts
@@ -1,5 +1,5 @@
 // from jcalz, https://stackoverflow.com/questions/50374908/transform-union-type-to-intersection-type/50375286#50375286
-
+// istanbul ignore file
 export type UnionToIntersection<U> = (U extends any ? (k: U) => void : never) extends ((k: infer I) => void) ? I : never
 
 export function toIntersection<U>(union: U): UnionToIntersection<U> {
