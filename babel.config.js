b580495e10a9b8018b147b29097821d3eb6eb6f8
unional <homawong@gmail.com>
2020-09-07 18:09:04 -0700

    chore:  '@babel/plugin-proposal-class-properties'

--- babel.config.js
+++ babel.config.js
@@ -13,6 +13,7 @@ module.exports = {
     }],
   ],
   plugins: [
+    '@babel/plugin-proposal-class-properties',
     '@babel/plugin-proposal-optional-chaining',
     '@babel/plugin-proposal-nullish-coalescing-operator'
   ],
