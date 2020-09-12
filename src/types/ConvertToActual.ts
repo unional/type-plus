4fb0c5d7983d8cbc2127ef315731e88a7b2d3a70
unional <homawong@gmail.com>
2020-09-12 15:26:24 -0700

    feat: add types.Null

--- src/types/ConvertToActual.ts
+++ src/types/ConvertToActual.ts
@@ -1,9 +1,11 @@
 import { AllTypes } from './AllTypes'
+import { Boolean, False, True } from './Boolean'
+import { Null } from './Null'
 import { Undefined } from './Undefined'
-import { Boolean, True, False } from './Boolean'
 
 export type ConvertToActual<T extends AllTypes> =
   T extends Undefined ? undefined :
+  T extends Null ? null :
   T extends True ? true :
   T extends False ? false :
   T extends Boolean ? boolean :
---
+++ src/types/Null.ts
@@ -0,0 +1,5 @@
+export type Null = { name: 'null' }
+
+export namespace Null {
+  export const name = 'null'
+}
