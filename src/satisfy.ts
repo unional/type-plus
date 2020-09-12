a317c150e2c20aeea6fcf8107fba7609da88b7f9
unional <homawong@gmail.com>
2020-09-12 16:33:03 -0700

    fix: boolean negative check

--- src/satisfy.ts
+++ src/satisfy.ts
@@ -3,10 +3,15 @@ import * as types from './types'
 export function satisfy<T extends types.AllTypes>(type: T, subject: unknown): subject is types.ConvertToActual<T> {
   switch (type.name) {
     case 'undefined':
-    case 'boolean':
     case 'number':
     case 'string':
       return typeof subject === type.name
+    case 'boolean': {
+      if (typeof subject !== 'boolean') return false
+      const b = type as types.Boolean
+      if (b === types.Boolean) return true
+      return b.value === 'true' ? subject : !subject
+    }
     case 'null':
       return subject === null
   }
