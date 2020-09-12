0da9111d8f50428e66eaac1878e16516cf300648
unional <homawong@gmail.com>
2020-09-12 15:55:07 -0700

    chore: add String type basic

--- src/types/ConvertToActual.ts
+++ src/types/ConvertToActual.ts
@@ -2,6 +2,7 @@ import { AllTypes } from './AllTypes'
 import { Boolean, False, True } from './Boolean'
 import { Null } from './Null'
 import { Number } from './Number'
+import { String } from './String'
 import { Undefined } from './Undefined'
 
 export type ConvertToActual<T extends AllTypes> =
@@ -11,4 +12,5 @@ export type ConvertToActual<T extends AllTypes> =
   T extends False ? false :
   T extends Boolean ? boolean :
   T extends Number ? T['value'] :
+  T extends String ? string :
   unknown
--- src/types/Number.ts
+++ src/types/Number.ts
@@ -5,7 +5,7 @@ export namespace Number {
   export const value = 0
 
   /**
-   * Creates a single value number.
+   * Creates a single value type.
    */
   export function val<N extends number>(value: N): Number<N> {
     // Cannot name this function as `const` because it is a reserved keyword.
