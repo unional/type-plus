dba7aee99e8439ea3c18196ee9887953c0ba9867
unional <homawong@gmail.com>
2020-09-12 15:18:24 -0700

    chore: simplify bool

--- src/types/Boolean.ts
+++ src/types/Boolean.ts
@@ -1,19 +1,20 @@
 import { Brand } from '../nominal'
 
-export type Boolean<
-  V extends 'true' | 'false' | '' = ''
-  > = V extends '' ? { name: 'boolean', value: 'true' | 'false' }
-  : Brand<{ name: 'boolean', value: V }, V>
+export type Boolean<Value extends 'true' | 'false' | '' = ''> = Value extends ''
+  ? { name: 'boolean', value: 'true' | 'false' }
+  : Brand<{ name: 'boolean', value: Value }, Value>
 
-export namespace Boolean {
-  export const name = 'boolean'
-  export const value = 'false'
-  export function fn(value: 'true'): True
-  export function fn(value: 'false'): False
-  export function fn(value: 'true' | 'false'): Boolean {
-    return { name: 'boolean', value }
-  }
-}
+export const Boolean: Boolean = { name: 'boolean', value: 'false' }
+
+// export namespace Boolean {
+//   export const name = 'boolean'
+//   export const value = 'false'
+//   export function fn(value: true): True
+//   export function fn(value: false): False
+//   export function fn(value: true | false): Boolean {
+//     return { name: 'boolean', value: value ? 'true' : 'false' }
+//   }
+// }
 
 export type True = Boolean<'true'>
 export const True: True = { name: 'boolean', value: 'true' } as any
