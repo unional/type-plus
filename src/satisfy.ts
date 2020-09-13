92a283868f2c7a6e85868acd51b8b392e3af440e
unional <homawong@gmail.com>
2020-09-12 17:08:43 -0700

    refactor: extract common code for satisfy

--- src/satisfy.ts
+++ src/satisfy.ts
@@ -2,34 +2,27 @@ import * as types from './types'
 
 export function satisfy<T extends types.AllTypes>(type: T, subject: unknown): subject is types.ConvertToActual<T> {
   switch (type.name) {
-    case 'undefined':
-      return typeof subject === type.name
-    case 'null':
-      return subject === null
+    case 'undefined': return typeof subject === type.name
+    case 'null': return subject === null
     case 'boolean': {
       if (typeof subject !== 'boolean') return false
       const t = type as types.Boolean
       if (t === types.Boolean) return true
       return t.value === 'true' ? subject : !subject
     }
-    case 'number': {
-      if (typeof subject !== 'number') return false
-      const t = type as types.Number
-      if (t === types.Number) return true
-      return subject === t.value
-    }
-    case 'string': {
-      if (typeof subject !== 'string') return false
-      const t = type as types.String
-      if (t === types.String) return true
-      return subject === t.value
-    }
-    case 'bigint': {
-      if (typeof subject !== 'bigint') return false
-      const t = type as types.BigInt
-      if (t === types.BigInt) return true
-      return subject === t.value
-    }
+    case 'number': return satisfyType(types.Number, type, subject)
+    case 'string': return satisfyType(types.String, type, subject)
+    case 'bigint': return satisfyType(types.BigInt, type, subject)
   }
   return false
+}
+
+function satisfyType(
+  check: types.Number | types.String | types.BigInt,
+  type: types.AllTypes,
+  subject: unknown
+) {
+  if (typeof subject !== check.name) return false
+  if (type === check) return true
+  return subject === (type as any).value
 }
