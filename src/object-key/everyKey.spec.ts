e72cc0febe6f57fa462e220d29ba2f742087b284
unional <homawong@gmail.com>
2020-09-13 23:06:49 -0700

    fix: type `this` arg for `everyKey()`

--- src/object-key/everyKey.spec.ts
+++ src/object-key/everyKey.spec.ts
@@ -4,3 +4,8 @@ test('predicate key can be used as indexer of the subject', () => {
   const subject = { a: 1, b: 2, c: 3 }
   expect(everyKey(subject, key => typeof subject[key] === 'number')).toBe(true)
 })
+
+test('type this arg', () => {
+  const subject = { b: 2 }
+  expect(everyKey(subject, function () { return this.a === 1 }, { a: 1 })).toBe(true)
+})
