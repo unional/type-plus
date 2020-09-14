d7543624cc6b9f9b51e25a54fa00558c08b4239e
unional <homawong@gmail.com>
2020-09-14 02:15:25 -0700

    chore: hide if

--- src/types/index.ts
+++ src/types/index.ts
@@ -10,7 +10,7 @@
 export * from './Any'
 export * from './Array'
 export * from './Boolean'
-export * from './If'
+// export * from './If'
 export { nil as null, Null } from './Null'
 export * from './Number'
 export * from './Object'
