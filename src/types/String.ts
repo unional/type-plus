096aec868aea9358fa7acf16d64dc2ce2b9b8508
unional <homawong@gmail.com>
2020-09-15 23:13:27 -0700

    feat: add string[.optional].list()

--- src/types/String.ts
+++ src/types/String.ts
@@ -1,7 +1,16 @@
+import { Tuple as TTTuple } from 'ts-toolbelt'
 import { undef, Undefined } from './Undefined'
 import { Union, union } from './Union'
 
 export type String<Value extends string = string> = { name: 'string', value: Value }
+
+type StringListDevice<Values extends string[]> = Values['length'] extends 0
+  ? { result: never }
+  : { result: String<Values[0]> | StringListDevice<TTTuple.Drop<Values, '1'>>['result'] }
+
+type StringOptionalListDevice<Values extends string[]> = Values['length'] extends 0
+  ? { result: Undefined }
+  : { result: String<Values[0]> | StringOptionalListDevice<TTTuple.Drop<Values, '1'>>['result'] }
 
 /**
  * Creates a constant string type.
@@ -14,6 +23,9 @@ function create<Value extends string>(value: Value): String<Value> {
 export const string = {
   ...create(undefined as unknown as string),
   create,
+  list<Values extends string[]>(...values: Values): StringListDevice<Values>['result'] {
+    return union.create(...values.map(create)) as any
+  },
   optional: {
     ...union.create(create(undefined as unknown as string), undef),
     /**
@@ -21,6 +33,9 @@ export const string = {
      */
     create<Value extends string>(value: Value): Union<[String<Value>, Undefined]> {
       return union.create(create(value), undef)
+    },
+    list<Values extends string[]>(...values: Values): StringOptionalListDevice<Values>['result'] {
+      return union.create(...values.map(create), undef) as any
     }
   }
 }
