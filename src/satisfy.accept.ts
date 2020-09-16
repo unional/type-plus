e776ada36d0b2cfe47116fcbcee8d8f7ec758e9c
unional <homawong@gmail.com>
2020-09-16 00:13:22 -0700

    chore: adding comment for alternative

--- src/satisfy.accept.ts
+++ src/satisfy.accept.ts
@@ -6,7 +6,22 @@ import { satisfy, types } from '.'
 const eslint = types.object.create({
   parseOptions: types.object.optional.create({
     ecmaVersion: types.number.optional.list(3, 5, 6, 7, 8, 9, 10, 11, 12),
+    // ecmaVersion2: types.union.optional.create(
+    //   types.number.create(3),
+    //   types.number.create(5),
+    //   types.number.create(6),
+    //   types.number.create(7),
+    //   types.number.create(8),
+    //   types.number.create(9),
+    //   types.number.create(10),
+    //   types.number.create(11),
+    //   types.number.create(12)
+    // ),
     sourceType: types.string.optional.list('script', 'module'),
+    // sourceType2: types.union.optional.create(
+    //   types.string.create('script'),
+    //   types.string.create('module')
+    // ),
     ecmaFeatures: types.object.optional.create({
       globalReturn: types.boolean.optional,
       impliedStrict: types.boolean.optional,
