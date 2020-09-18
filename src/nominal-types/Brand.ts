37fdfe0113fcb61bd07e8527952d22a19d18c87c
unional <homawong@gmail.com>
2020-09-17 23:59:23 -0700

    feat: add brand()

--- src/nominal-types/Brand.ts
+++ src/nominal-types/Brand.ts
@@ -1,11 +1,18 @@
 // By Drew Colthorp, <https://spin.atomicobject.com/2018/01/15/typescript-flexible-nominal-typing/#comment-604580>
 // <https://gist.github.com/dcolthorp/aa21cf87d847ae9942106435bf47565d>
 
+import { AnyRecord } from '../any-types'
+
 /**
  * Create a "branded" version of a type.
  * TypeScript won't allow implicit conversion to this type
  */
-export type Brand<BrandT extends string, T> = T & { _type: BrandT }
+export type Brand<BrandT extends string, T extends AnyRecord> = T & { _type: BrandT }
+
+export function brand<B extends string, T extends AnyRecord>(type: B, subject?: T): Brand<B, T> {
+  (subject as any)._type = type
+  return subject as any
+}
 
 export function createBrandCreator<BrandT extends string, T>(): (value: T) => Brand<BrandT, T> {
   return (value: T): Brand<BrandT, T> => {
