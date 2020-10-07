ed39e39cd878fbf2ddc37b5d9e3bcfa3846b405a
unional <homawong@gmail.com>
2020-10-06 21:40:49 -0700

    refactor: eliminate ExtractType

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,8 +1,3 @@
 export type ValueType<T extends string, Value> = { type: T, value: Value }
 
 export type AnalysisType<T extends string, V = never> = { type: T, value?: V, fail?: true }
-
-export type ExtractType<V extends ValueType<any,any>> = {
-  type: V['type'],
-  value: V['value']
-}
