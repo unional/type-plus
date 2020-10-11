99829195348283fb52e1da199121235a6d7c98cb
unional <homawong@gmail.com>
2020-10-11 15:04:05 -0700

    feat: assertType() to support unknown with validator
    BREAKING CHANGE: remove assertUnknown
    
    assertType() provides the same functionality

--- src/assertion/assertType.ts
+++ src/assertion/assertType.ts
@@ -1,4 +1,6 @@
 
+import { tersify } from 'tersify'
+import { isConstructor } from '../class'
 import { AnyFunction } from '../function'
 
 /**
@@ -6,79 +8,92 @@ import { AnyFunction } from '../function'
  * @type T the type to check against.
  */
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export function assertType<T>(subject: T): void { return }
-// this does not work at the moment (TypeScript 3.7.3)
-// export function assertType<T, U extends T = T>(subject: U): asserts subject is T { return }
+export function assertType<T>(subject: T): asserts subject is T
+export function assertType<T>(subject: unknown, validator: (s: T) => boolean): asserts subject is T
+export function assertType<T extends new (...args: any) => any>(subject: unknown, constructor: T): asserts subject is InstanceType<T>
+export function assertType(subject: any, validator?: (s: any) => boolean) {
+  if (validator) {
+    if ((isConstructor(validator) && !(subject instanceof validator)) || !validator(subject as any))
+      throw new TypeError(`subject fails to satisfy ${tersify(validator)}`)
+  }
+  return
+}
+// this does not work at the moment (TypeScript 4.0.2)
+// export function assertType2<T, S extends T = T>(subject: S): asserts subject is T { return }
 
-assertType.isUndefined = function (value: undefined): asserts value is undefined {
-  if (typeof value !== 'undefined') throw TypeError(`value is not undefined`)
+assertType.isUndefined = function (subject: undefined): asserts subject is undefined {
+  if (typeof subject !== 'undefined') throw TypeError(`subject is not undefined`)
 }
-assertType.noUndefined = function <S>(value: Exclude<S, undefined>): void {
-  if (typeof value === 'undefined') throw TypeError(`value is undefined`)
-}
-
-assertType.isNull = function (value: null): asserts value is null {
-  if (value !== null) throw TypeError(`value is not null`)
-}
-assertType.noNull = function <S>(value: Exclude<S, null>): void {
-  if (value === null) throw TypeError(`value is null`)
-}
-
-assertType.isNumber = function (value: number): asserts value is number {
-  if (typeof value !== 'number') throw TypeError(`value is not number`)
-}
-assertType.noNumber = function <S>(value: Exclude<S, number>): void {
-  if (typeof value === 'number') throw TypeError(`value is number`)
+assertType.noUndefined = function <S>(subject: Exclude<S, undefined>): void {
+  if (typeof subject === 'undefined') throw TypeError(`subject is undefined`)
 }
 
-assertType.isBoolean = function (value: boolean): asserts value is boolean {
-  if (typeof value !== 'boolean') throw TypeError(`value is not boolean`)
+assertType.isNull = function (subject: null): asserts subject is null {
+  if (subject !== null) throw TypeError(`subject is not null`)
 }
-assertType.noBoolean = function <S>(value: Exclude<S, boolean>): void {
-  if (typeof value === 'boolean') throw TypeError(`value is boolean`)
+assertType.noNull = function <S>(subject: Exclude<S, null>): void {
+  if (subject === null) throw TypeError(`subject is null`)
 }
 
-assertType.isTrue = function (value: true): asserts value is true {
-  if (value !== true) throw TypeError(`value is not true`)
+assertType.isNumber = function (subject: number): asserts subject is number {
+  if (typeof subject !== 'number') throw TypeError(`subject is not number`)
 }
-assertType.noTrue = function <S>(value: Exclude<S, true>): void {
+assertType.noNumber = function <S>(subject: Exclude<S, number>): void {
+  if (typeof subject === 'number') throw TypeError(`subject is number`)
+}
+
+assertType.isBoolean = function (subject: boolean): asserts subject is boolean {
+  if (typeof subject !== 'boolean') throw TypeError(`subject is not boolean`)
+}
+assertType.noBoolean = function <S>(subject: Exclude<S, boolean>): void {
+  if (typeof subject === 'boolean') throw TypeError(`subject is boolean`)
+}
+
+assertType.isTrue = function (subject: true): asserts subject is true {
+  if (subject !== true) throw TypeError(`subject is not true`)
+}
+assertType.noTrue = function <S>(subject: Exclude<S, true>): void {
   // @ts-ignore
-  if (value === true) throw TypeError(`value is true`)
+  if (subject === true) throw TypeError(`subject is true`)
 }
 
-assertType.isFalse = function (value: false): asserts value is false {
-  if (value !== false) throw TypeError(`value is not false`)
+assertType.isFalse = function (subject: false): asserts subject is false {
+  if (subject !== false) throw TypeError(`subject is not false`)
 }
-assertType.noFalse = function <S>(value: Exclude<S, false>): void {
+assertType.noFalse = function <S>(subject: Exclude<S, false>): void {
   // @ts-ignore
-  if (value === false) throw TypeError(`value is false`)
+  if (subject === false) throw TypeError(`subject is false`)
 }
 
-assertType.isString = function (value: string): asserts value is string {
-  if (typeof value !== 'string') throw TypeError(`value is not string`)
+assertType.isString = function (subject: string): asserts subject is string {
+  if (typeof subject !== 'string') throw TypeError(`subject is not string`)
 }
-assertType.noString = function <S>(value: Exclude<S, string>): void {
-  if (typeof value === 'string') throw TypeError(`value is string`)
-}
-
-assertType.isFunction = function (value: AnyFunction): asserts value is AnyFunction {
-  if (typeof value !== 'function') throw TypeError(`value is not function`)
-}
-assertType.noFunction = function <S>(value: Exclude<S, AnyFunction>): void {
-  if (typeof value === 'function') throw TypeError(`value is function`)
+assertType.noString = function <S>(subject: Exclude<S, string>): void {
+  if (typeof subject === 'string') throw TypeError(`subject is string`)
 }
 
-assertType.isError = function (value: Error): asserts value is Error {
-  if (!(value instanceof Error)) throw TypeError(`value is not an Error`)
+assertType.isFunction = function (subject: AnyFunction): asserts subject is AnyFunction {
+  if (typeof subject !== 'function') throw TypeError(`subject is not function`)
 }
-assertType.noError = function <S>(value: Exclude<S, Error>): void {
-  if (value instanceof Error) throw TypeError(`value is an Error`)
+assertType.noFunction = function <S>(subject: Exclude<S, AnyFunction>): void {
+  if (typeof subject === 'function') throw TypeError(`subject is function`)
+}
+
+assertType.isError = function (subject: Error): asserts subject is Error {
+  if (!(subject instanceof Error)) throw TypeError(`subject is not an Error`)
+}
+assertType.noError = function <S>(subject: Exclude<S, Error>): void {
+  if (subject instanceof Error) throw TypeError(`subject is an Error`)
 }
 
 /**
- * create a type assertion function for the specified type.
- * @type T the type to check against.
+ * creates a custom assertion function with standard TypeError.
  */
-export function typeAssertion<T>(): <R extends T>(subject: R) => R {
-  return (subject) => subject as any
+assertType.custom = function <T>(
+  validator: (s: T) => boolean
+): (subject: unknown) => asserts subject is T {
+  return function (subject) {
+    if (!validator(subject as any)) throw new TypeError(`subject fails to satisfy ${tersify(validator)}`)
+    return
+  }
 }
--- src/assertion/assertUnknown.ts
+++
@@ -1,8 +0,0 @@
-/**
- * assert the subject satisfies the specified type T
- * @type T the type to check against.
- */
-export function assertUnknown<T>(value: unknown, handler?: (s: T) => boolean): asserts value is T {
-  if (handler && !handler(value as any)) throw new TypeError(`fail to assert value through handler`)
-  return
-}
