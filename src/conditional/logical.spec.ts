74cc46e7d28cf3e6f1e6e4f2d875307527138785
unional <homawong@gmail.com>
2020-10-12 01:07:42 -0700

    fix: logical types with boolean type

--- src/conditional/logical.spec.ts
+++ src/conditional/logical.spec.ts
@@ -1,28 +1,58 @@
 import { And, assertType, Not, Or } from '..'
+import { Equal } from '../assertion'
 import { Xor } from './logical'
 
-test('And<A,B>', () => {
-  assertType.isTrue(true as And<true, true>)
-  assertType.isFalse(false as And<true, false>)
-  assertType.isFalse(false as And<false, true>)
-  assertType.isFalse(false as And<false, false>)
+describe('And<A,B>', () => {
+  test('basic', () => {
+    assertType.isTrue(true as And<true, true>)
+    assertType.isFalse(false as And<true, false>)
+    assertType.isFalse(false as And<false, true>)
+    assertType.isFalse(false as And<false, false>)
+  })
+  test('boolean special handling', () => {
+    assertType.isFalse(false as And<boolean, true>)
+    assertType.isFalse(false as And<boolean, false>)
+    assertType.isFalse(false as And<true, boolean>)
+    assertType.isFalse(false as And<false, boolean>)
+  })
 })
 
-test('Or<A,B>', () => {
-  assertType.isTrue(true as Or<true, true>)
-  assertType.isTrue(true as Or<true, false>)
-  assertType.isTrue(true as Or<false, true>)
-  assertType.isFalse(false as Or<false, false>)
+describe('Or<A,B>', () => {
+  test('basic', () => {
+    assertType.isTrue(true as Or<true, true>)
+    assertType.isTrue(true as Or<true, false>)
+    assertType.isTrue(true as Or<false, true>)
+    assertType.isFalse(false as Or<false, false>)
+  })
+  test('boolean special handling', () => {
+    assertType.isTrue(true as Or<boolean, true>)
+    assertType.isFalse(false as Or<boolean, false>)
+    assertType.isTrue(true as Or<true, boolean>)
+    assertType.isFalse(false as Or<false, boolean>)
+  })
 })
 
-test('Xor<A,B>', () => {
-  assertType.isFalse(false as Xor<true, true>)
-  assertType.isTrue(true as Xor<true, false>)
-  assertType.isTrue(true as Xor<false, true>)
-  assertType.isFalse(false as Xor<false, false>)
+describe('Xor<A,B>', () => {
+  test('basic', () => {
+    assertType.isFalse(false as Xor<true, true>)
+    assertType.isTrue(true as Xor<true, false>)
+    assertType.isTrue(true as Xor<false, true>)
+    assertType.isFalse(false as Xor<false, false>)
+  })
+  test('boolean special handling', () => {
+    assertType.isTrue(true as Equal<Xor<boolean, true>, boolean>)
+    assertType.isTrue(true as Equal<Xor<boolean, false>, boolean>)
+    assertType.isTrue(true as Equal<Xor<true, boolean>, boolean>)
+    assertType.isTrue(true as Equal<Xor<false, boolean>, boolean>)
+  })
 })
 
-test('Not<X>', () => {
-  assertType.isTrue(true as Not<false>)
-  assertType.isFalse(false as Not<true>)
+describe('Not<X>', () => {
+  test('basic', () => {
+    assertType.isTrue(true as Not<false>)
+    assertType.isFalse(false as Not<true>)
+  })
+  test('boolean special handling', () => {
+    assertType.isTrue(true as Equal<Not<boolean>, boolean>)
+  })
 })
