7740834aff0163760c5e7d35b8f34680f1d4e415
unional <homawong@gmail.com>
2020-10-11 18:15:31 -0700

    feat: add createId() overload
    BREAKING CHANGE: remove createIdCreator
    
    Use `createId(type)` instead.

--- src/nominal-types/Id.ts
+++ src/nominal-types/Id.ts
@@ -2,25 +2,20 @@
 // https://basarat.gitbooks.io/typescript/docs/tips/nominalTyping.html
 
 /**
- * Generic Id type
+ * A generic Id type
  */
 export type Id<T extends string> = {
   type: T,
   value: string,
 }
 
-export function createId<T extends string>(type: T, value: string): Id<T> {
-  return {
-    type,
-    value,
-  }
-}
-
-export function createIdCreator<T extends string>(type: T) {
-  return function createId(value: string): Id<T> {
-    return {
-      type,
-      value,
+export function createId<T extends string>(type: T): (value: string) => Id<T>
+export function createId<T extends string>(type: T, value: string): Id<T>
+export function createId<T extends string>(type: T, value?: string) {
+  if (value === undefined) {
+    return function (value: string) {
+      return { type, value }
     }
   }
+  return { type, value }
 }
