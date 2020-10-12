fe98601561fca06ae6f3105bf6c2fbff3687fd7b
unional <homawong@gmail.com>
2020-10-11 18:28:58 -0700

    feat: add Widen<T>
    This is the same type from `typical`
    /cc @tycho01

---
+++ src/utils/Widen.ts
@@ -0,0 +1,9 @@
+/**
+ * Widen scalar types from literals to their parent types.
+ * Borrow from `typical`
+ */
+export type Widen<T> =
+  T extends boolean ? boolean :
+  T extends number ? number :
+  T extends string ? string :
+  T
