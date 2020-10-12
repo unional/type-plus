8d496ebacf1ecbaaa8d8d2c128f551f456185911
Homa Wong <homawong@gmail.com>
2020-10-12 10:56:54 -0700

    refactor: improve `Not`

--- src/conditional/logical.ts
+++ src/conditional/logical.ts
@@ -6,11 +6,13 @@ export type And<A extends boolean, B extends boolean> = If<
   If<Equal<B, true>, true, false>,
   false
 >
+
 export type Or<A extends boolean, B extends boolean> = If<
   Equal<A, true>,
   true,
   If<Equal<B, true>, true, false>
 >
+
 export type Xor<A extends boolean, B extends boolean> = If<
   Or<Equal<A, boolean>, Equal<B, boolean>>,
   boolean,
@@ -20,8 +22,13 @@ export type Xor<A extends boolean, B extends boolean> = If<
     If<Equal<B, true>, true, false>
   >
 >
+
+/**
+ * Logical NOT operator.
+ * This version is proposed by @Constantiner
+ */
 export type Not<X extends boolean> = If<
-  Equal<X, boolean>,
-  boolean,
-  If<Equal<X, true>, false, true>
+  Equal<X, true>,
+  false,
+  If<Equal<X, false>, true, boolean>
 >
