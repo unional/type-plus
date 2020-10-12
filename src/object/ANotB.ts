070b5d6383aeb9bb883506362cddc826d8f4037a
unional <homawong@gmail.com>
2020-10-12 00:27:27 -0700

    test: fix ANotB

--- src/object/ANotB.ts
+++ src/object/ANotB.ts
@@ -1,22 +1,30 @@
-import { IsSame } from '../conditional'
 import { KeysWithDiffType } from '.'
+import { TypeEquals } from '../assertion'
+import { If } from '../conditional'
 import { AnyRecord } from './AnyRecord'
 import { IsDisjoint } from './IsDisjoint'
 
 export type ANotB<A extends AnyRecord, B extends AnyRecord> =
-  IsSame<A, B> extends true ? never :
-  IsDisjoint<A, B> extends true ? A :
-  {
-    [k in Exclude<keyof A, keyof B> | KeysWithDiffType<A, B>]: A[k]
-  }
+  If<
+    TypeEquals<A, B>,
+    never,
+    If<
+      IsDisjoint<A, B>,
+      A,
+      { [k in Exclude<keyof A, keyof B> | KeysWithDiffType<A, B>]: A[k] }
+    >
+  >
 
 export type BNotA<A extends AnyRecord, B extends AnyRecord> = ANotB<B, A>
 
 export type LeftJoin<A extends AnyRecord, B extends AnyRecord> =
-  IsSame<A, B> extends true ? A :
-  IsDisjoint<A, B> extends true ? A & B :
-  {
-    [k in Exclude<keyof A, keyof B>]: A[k]
-  } & {
-    [k in keyof B]: B[k]
-  }
+  If<
+    TypeEquals<A, B>,
+    A,
+    If<
+      IsDisjoint<A, B>,
+      A & B,
+      { [k in Exclude<keyof A, keyof B>]: A[k] }
+      & { [k in keyof B]: B[k] }
+    >
+  >
