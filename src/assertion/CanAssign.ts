b926aeb69b13740d2be526619f8e3ca26eeb23fa
unional <homawong@gmail.com>
2020-10-12 00:23:51 -0700

    chore: add comment

--- src/assertion/CanAssign.ts
+++ src/assertion/CanAssign.ts
@@ -1,10 +1,17 @@
 import { If, Or } from '../conditional'
 import { TypeEquals, TypeNotEquals } from './TypeEquals'
 
+/**
+ * Can `A` assign to `B`
+ */
 export type CanAssign<A, B> = If<
+  // boolean extends true (or false) -> boolean because
+  // boolean === true | false.
+  // as it is a finite set.
+  // so special handling is needed.
   TypeEquals<A, boolean>,
   If<
-    Or<Or<TypeEquals<B, false>, TypeEquals<B, true>>, TypeNotEquals<B, boolean>>,
+    Or<Or<TypeEquals<B, true>, TypeEquals<B, false>>, TypeNotEquals<B, boolean>>,
     false,
     true
   >,
