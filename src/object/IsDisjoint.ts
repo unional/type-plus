ea59a357bac54c4326d3b912bd7164c966ec080e
unional <homawong@gmail.com>
2020-10-12 20:18:34 -0700

    fix: use AndS and NotS for IsDisjoint for the time being
    It is not 100% correct, but bettern the And and Not for now.

--- src/object/IsDisjoint.ts
+++ src/object/IsDisjoint.ts
@@ -1,8 +1,14 @@
-import { And, Not } from '../conditional'
+import { AndS, NotS } from '../conditional'
 import { AnyRecord } from './AnyRecord'
 import { HasKey } from './hasKey'
 
+/**
+ * Is the two record disjoint form each other.
+ * Disjoint means no common property.
+ * Note: using `AndS` and `NotS` due to:
+ * <https://github.com/microsoft/TypeScript/issues/41053>
+ */
 export type IsDisjoint<
   A extends AnyRecord,
   B extends AnyRecord
-  > = And<Not<HasKey<A, keyof B>>, Not<HasKey<B, keyof A>>>
+  > = AndS<NotS<HasKey<A, keyof B>>, NotS<HasKey<B, keyof A>>>
