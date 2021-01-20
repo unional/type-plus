dc11ce4fd604e4b8fb79de2a6045a74c87d6a7db
unional <homawong@gmail.com>
2021-01-20 00:25:29 -0800

    fix: First<> returns `never` in edge case
    This comes up when the input is a generic array in certain case.

--- src/array/First.ts
+++ src/array/First.ts
@@ -1,11 +1,12 @@
 import { ArrayValue } from './ArrayValue'
 
-export type First<
-  A extends Array<any>,
-  Criteria> =
-  number extends A['length'] ?
-  ArrayValue<A> extends Criteria ? ArrayValue<A> : never :
-  A['length'] extends 0 ? never :
-  A extends [infer X, ...infer Tail] ?
-  X extends Criteria ? X :
-  First<Tail, Criteria> : A
+export type First<A extends Array<any>, Criteria> =
+  number extends A['length']
+  ? (ArrayValue<A> extends Criteria ? ArrayValue<A> : never)
+  : (A['length'] extends 0
+    ? never
+    : (A extends [infer X, ...infer Tail]
+      ? (X extends Criteria ? X : First<Tail, Criteria>)
+      : never
+    )
+  )
