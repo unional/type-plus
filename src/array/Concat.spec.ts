cd039775fb039cb3c028b942d92dd9d8fbdb7cd2
unional <homawong@gmail.com>
2021-01-22 00:45:05 -0800

    feat: add Concat<A, B>

---
+++ src/array/Concat.spec.ts
@@ -0,0 +1,22 @@
+import { Concat } from '..'
+import { Equal, isType } from '../assertion'
+
+test('concat array', () => {
+  type A = Concat<string[], boolean[]>
+  isType.true<Equal<Array<string | boolean>, A>>()
+})
+
+test('concat tuples', () => {
+  type A = Concat<[1, 2, 3], [4, 5]>
+  isType.true<Equal<[1, 2, 3, 4, 5], A>>()
+})
+
+test('concat array to tuple', () => {
+  type A = Concat<string[], [1, 2, 3]>
+  isType.true<Equal<Array<string | 1 | 2 | 3>, A>>()
+})
+
+test('concat tuple to array', () => {
+  type A = Concat<[1, 2, 3], string[]>
+  isType.true<Equal<[1, 2, 3, ...string[]], A>>()
+})
---
+++ src/array/Concat.ts
@@ -0,0 +1,1 @@
+export type Concat<A extends any[], B extends any[]> = [...A, ...B]
