a634cea05441273cd7333374bf083fa4aee309fb
unional <homawong@gmail.com>
2021-01-22 23:39:21 -0800

    fix: Tail to preserve type during generics

--- src/array/Tail.spec.ts
+++ src/array/Tail.spec.ts
@@ -1,21 +1,33 @@
 import { assertType, Equal, Tail } from '..'
+import { isType } from '../assertion'
 
 test('get tail types', () => {
   type S = [1, 'a', 'b']
   type A = Tail<S>
 
-  assertType.isTrue(true as Equal<A, ['a', 'b']>)
+  isType.equal<true, ['a', 'b'], A>()
 })
 
 test('empty tuple gets never', () => {
   type S = []
   type A = Tail<S>
 
-  assertType.isTrue(true as Equal<A, never>)
+  isType.equal<true, never, A>()
 })
 
 test('array gets same type', () => {
   type A = Tail<string[]>
 
-  assertType.isTrue(true as Equal<A, string[]>)
+  isType.equal<true, string[], A>()
+})
+
+test('union array', () => {
+  type A = Tail<Array<string | boolean>>
+
+  isType.equal<true, Array<string | boolean>, A>()
+})
+
+test('tuple with rest', () => {
+  type A = Tail<[number, ...string[]]>
+  isType.equal<true, string[], A>()
 })
