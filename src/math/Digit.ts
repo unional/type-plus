e87bdff32d679d2f6a14ffc1de426392462152c6
unional <homawong@gmail.com>
2021-01-23 14:29:59 -0800

    chore: add Digit.GreaterThan<A,B>
    Currently only work for 0-9, may expand to 0-19

--- src/math/Digit.ts
+++ src/math/Digit.ts
@@ -1,3 +1,4 @@
+import { Tail } from '../array'
 import { And } from '../conditional'
 import { IsPositive } from './IsPositive'
 import { IsWhole } from './IsWhole'
@@ -24,14 +25,33 @@ export namespace Digit {
     15: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     16: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     17: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    18: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    18: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    19: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  }
+
+  /**
+   * A > B for [1-9]
+   */
+  export type GreaterThan<A extends number, B extends number> = B extends GreaterThanMap[A] ? true : false
+
+  type GreaterThanMap = { [k in number]: number } & {
+    0: never,
+    1: 0,
+    2: 0 | 1,
+    3: 0 | 1 | 2,
+    4: 0 | 1 | 2 | 3,
+    5: 0 | 1 | 2 | 3 | 4,
+    6: 0 | 1 | 2 | 3 | 4 | 5,
+    7: 0 | 1 | 2 | 3 | 4 | 5 | 6,
+    8: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7,
+    9: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8,
   }
 }
 
 export namespace DigitArray {
   export type ToNumber<C extends number[]> = ToTuple<[], C>['length']
 
-  type ToTuple<R extends any[], S extends number[]> = (
+  export type ToTuple<R extends any[], S extends number[]> = (
     S['length'] extends 0
     ? R
     : (S['length'] extends 1
@@ -63,4 +83,35 @@ export namespace DigitArray {
     : S extends `9${infer L}` ? [9, ...StringToDigitArray<L>]
     : S extends `0${infer L}` ? [0, ...StringToDigitArray<L>]
     : []
+
+  export type Shift10<DA extends number[]> = TrimLeadingZero<Shift10State<[], DA>>
+  type Shift10State<R extends number[], DA extends number[]> =
+    DA['length'] extends 0
+    ? R
+    : (DA['length'] extends 1
+      ? [...R, ...DA]
+      : (DA extends [any, ...infer T]
+        ? (T extends number[]
+          ? (DA[0] extends 0
+            ? Shift10State<[...R, DA[0]], T>
+            : (T extends [any, ...infer Y]
+              ? (Y extends number[]
+                ? Shift10State<[...R, MinusOne[DA[0]]], [Plus10[T[0]], ...Y]>
+                : [])
+              : []))
+          : [])
+        : [])
+    )
+
+  type TrimLeadingZero<A extends number[]> = A['length'] extends 0
+    ? A
+    : A[0] extends 0 ? TrimLeadingZero<Tail<A>> : A
+
+  type MinusOne = { [k in number]: number } & {
+    0: 0, 1: 0, 2: 1, 3: 2, 4: 3, 5: 4, 6: 5, 7: 6, 8: 7, 9: 8,
+    10: 9, 11: 10, 12: 11, 13: 12, 14: 13, 15: 14, 16: 15, 17: 16, 18: 17, 19: 18
+  }
+
+  type Plus10 = { [k in number]: number } &
+  { 0: 10, 1: 11, 2: 12, 3: 13, 4: 14, 5: 15, 6: 16, 7: 17, 8: 18, 9: 19 }
 }
