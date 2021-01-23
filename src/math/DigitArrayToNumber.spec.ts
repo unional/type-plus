c48eb7ea5accf7f6f897eec62bea1bc7055afe52
Homa Wong <homawong@gmail.com>
2021-01-22 17:44:07 -0800

    chore: finalize DigitArrayToNumber
    this version has good performance

--- src/math/DigitArrayToNumber.spec.ts
+++ src/math/DigitArrayToNumber.spec.ts
@@ -1,33 +1,34 @@
 import { Equal, isType } from '../assertion'
-import { DigitArrayToNumber2 } from './DigitArrayToNumber'
+import { DigitArrayToNumber } from './DigitArrayToNumber'
 
 test('no digit gets 0', () => {
-  type A = DigitArrayToNumber2<[]>
+  type A = DigitArrayToNumber<[]>
   isType.true<Equal<A, 0>>()
 })
 
 test('single digit', () => {
-  isType.true<Equal<DigitArrayToNumber2<[0]>, 0>>()
-  isType.true<Equal<DigitArrayToNumber2<[1]>, 1>>()
-  isType.true<Equal<DigitArrayToNumber2<[2]>, 2>>()
-  isType.true<Equal<DigitArrayToNumber2<[3]>, 3>>()
-  isType.true<Equal<DigitArrayToNumber2<[4]>, 4>>()
-  isType.true<Equal<DigitArrayToNumber2<[5]>, 5>>()
-  isType.true<Equal<DigitArrayToNumber2<[6]>, 6>>()
-  isType.true<Equal<DigitArrayToNumber2<[7]>, 7>>()
-  isType.true<Equal<DigitArrayToNumber2<[8]>, 8>>()
-  isType.true<Equal<DigitArrayToNumber2<[9]>, 9>>()
+  isType.true<Equal<DigitArrayToNumber<[0]>, 0>>()
+  isType.true<Equal<DigitArrayToNumber<[1]>, 1>>()
+  isType.true<Equal<DigitArrayToNumber<[2]>, 2>>()
+  isType.true<Equal<DigitArrayToNumber<[3]>, 3>>()
+  isType.true<Equal<DigitArrayToNumber<[4]>, 4>>()
+  isType.true<Equal<DigitArrayToNumber<[5]>, 5>>()
+  isType.true<Equal<DigitArrayToNumber<[6]>, 6>>()
+  isType.true<Equal<DigitArrayToNumber<[7]>, 7>>()
+  isType.true<Equal<DigitArrayToNumber<[8]>, 8>>()
+  isType.true<Equal<DigitArrayToNumber<[9]>, 9>>()
 
 })
 
 test('two digits', () => {
-  isType.true<Equal<DigitArrayToNumber2<[1, 0]>, 10>>()
-  isType.true<Equal<DigitArrayToNumber2<[1, 0]>, 10>>()
-  isType.true<Equal<DigitArrayToNumber2<[3, 1]>, 31>>()
-  isType.true<Equal<DigitArrayToNumber2<[13, 1]>, 131>>()
+  isType.true<Equal<DigitArrayToNumber<[1, 0]>, 10>>()
+  isType.true<Equal<DigitArrayToNumber<[1, 0]>, 10>>()
+  isType.true<Equal<DigitArrayToNumber<[3, 1]>, 31>>()
+  isType.true<Equal<DigitArrayToNumber<[13, 1]>, 131>>()
+  isType.true<Equal<DigitArrayToNumber<[1, 2, 3, 4]>, 1234>>()
 })
 
-// test('multi digits', () => {
-//   isType.true<Equal<DigitArrayToNumber2<[1, 2, 3, 4, 5]>, 12345>>()
-//   isType.true<Equal<DigitArrayToNumber2<[1, 2, 3, 4, 15]>, 12355>>()
-// })
+test('multi digits', () => {
+  isType.true<Equal<DigitArrayToNumber<[1, 2, 3, 4, 5]>, 12345>>()
+  isType.true<Equal<DigitArrayToNumber<[1, 2, 3, 4, 15]>, 12355>>()
+})
