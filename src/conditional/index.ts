986d142c8812b5c5ddcc634af6294e71422b8e32
unional <homawong@gmail.com>
2021-01-23 21:27:25 -0800

    feat: IsBoolean<T>

--- src/conditional/index.ts
+++ src/conditional/index.ts
@@ -1,6 +1,10 @@
+import { Equal } from '../assertion'
+
 export * from './If'
 export * from './logical'
 
 export type IsLiteral<T extends number | string> = number extends T ?
   false :
   string extends T ? false : true
+
+export type IsBoolean<T> = Equal<T, boolean> extends true ? true : false
