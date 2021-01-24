ff906568cb5a94d2c37112f425eadaa15c71f385
unional <homawong@gmail.com>
2021-01-23 22:43:26 -0800

    feat: add Then/Else to IsLiterial and IsBoolean

--- src/conditional/index.ts
+++ src/conditional/index.ts
@@ -1,8 +1,8 @@
 export * from './If'
 export * from './logical'
 
-export type IsLiteral<T extends number | string> = number extends T ?
-  false :
-  string extends T ? false : true
+export type IsLiteral<T extends number | string, Then = true, Else = false> = number extends T ?
+  Else :
+  string extends T ? Else : Then
 
-export type IsBoolean<T> = boolean extends T ? true : false
+export type IsBoolean<T, Then = true, Else = false> = boolean extends T ? Then : Else
