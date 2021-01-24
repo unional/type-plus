a86ae357480e3002aedfca78af5592c652ab5bfe
unional <homawong@gmail.com>
2021-01-23 22:37:49 -0800

    fix: add Fail cases for Add, Increment, Decrement

--- src/math/Add.ts
+++ src/math/Add.ts
@@ -7,32 +7,32 @@ import { IsWhole } from './IsWhole'
 /**
  * Add two number literals.
  */
-export type Add<A extends number, B extends number> = And<
-  And<IsPositive<A>, IsWhole<A>>,
-  And<IsPositive<B>, IsWhole<B>>
-> extends true
-  ? DigitArray.ToNumber<
+export type Add<A extends number, B extends number, Fail = never> =
+  And<
+    And<IsPositive<A>, IsWhole<A>>,
+    And<IsPositive<B>, IsWhole<B>>
+  > extends false ? Fail :
+  DigitArray.ToNumber<
     Reverse<AddDigitArray<
       Reverse<DigitArray.FromNumber<A>>,
       Reverse<DigitArray.FromNumber<B>>
     >>
   >
-  : never
 
 type AddDigitArray<A extends number[], B extends number[]> = (
-  A extends [any, ...infer AT]
-  ? (AT extends number[]
-    ? (B extends [any, ...infer BT]
-      ? (BT extends number[]
-        ? [AddDigit<A[0], B[0]>, ...AddDigitArray<AT, BT>]
-        : [])
-      : [A[0], ...AddDigitArray<[], AT>])
-    : [])
-  : (B extends [any, ...infer BT]
-    ? (BT extends number[]
-      ? [B[0], ...AddDigitArray<[], BT>]
-      : [])
-    : [])
+  A extends [any, ...infer ATail] ?
+  (ATail extends number[] ?
+    (B extends [any, ...infer BTail] ?
+      (BTail extends number[] ?
+        [AddDigit<A[0], B[0]>, ...AddDigitArray<ATail, BTail>] :
+        []) :
+      [A[0], ...AddDigitArray<[], ATail>]) :
+    []) :
+  (B extends [any, ...infer BTail] ?
+    (BTail extends number[] ?
+      [B[0], ...AddDigitArray<[], BTail>] :
+      []) :
+    [])
 )
 
 type AddDigit<A extends number, B extends number> = [...Digit.ToTuple[A], ...Digit.ToTuple[B]]['length']
@@ -40,4 +40,4 @@ type AddDigit<A extends number, B extends number> = [...Digit.ToTuple[A], ...Dig
 /**
  * Increment `A` by 1.
  */
-export type Increment<A extends number> = Add<A, 1>
+export type Increment<A extends number, Fail = never> = Add<A, 1, Fail>
