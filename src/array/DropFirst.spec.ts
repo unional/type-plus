d613aec45c0ad98141901cd0ef82554b2c8c0362
unional <homawong@gmail.com>
2021-01-24 19:09:01 -0800

    test: add tests for DropFirst/Last
    for empty array

--- src/array/DropFirst.spec.ts
+++ src/array/DropFirst.spec.ts
@@ -1,17 +1,21 @@
-import { assertType, Equal, DropFirst } from '..'
+import { DropFirst, isType } from '..'
+
+test('drop from empty gets itself', () => {
+  isType.equal<true, string[], DropFirst<string[]>>()
+})
 
 test('drop first value type from tuple', () => {
   type Actual = DropFirst<[true, 1, 'x', 3]>
-  assertType.isTrue(true as Equal<[1, 'x', 3], Actual>)
+  isType.equal<true, [1, 'x', 3], Actual>()
 })
 
 test('drop only value gets never[]', () => {
   type Actual = DropFirst<['x']>
-  assertType.isTrue(true as Equal<never[], Actual>)
+  isType.equal<true, never[], Actual>()
 })
 
 test('array type gets itself', () => {
   const s: string[] = ['a', 'b', 'c']
   type Actual = DropFirst<typeof s>
-  assertType.isTrue(true as Equal<string[], Actual>)
+  isType.equal<true, string[], Actual>()
 })
--- src/array/DropLast.spec.ts
+++ src/array/DropLast.spec.ts
@@ -1,17 +1,21 @@
-import { assertType, Equal, DropLast } from '..'
+import { DropLast, isType } from '..'
+
+test('drop from empty gets itself', () => {
+  isType.equal<true, string[], DropLast<string[]>>()
+})
 
 test('drop first value type from tuple', () => {
   type Actual = DropLast<[true, 1, 'x', 3]>
-  assertType.isTrue(true as Equal<[true, 1, 'x'], Actual>)
+  isType.equal<true, [true, 1, 'x'], Actual>()
 })
 
 test('drop only value gets never[]', () => {
   type Actual = DropLast<['x']>
-  assertType.isTrue(true as Equal<never[], Actual>)
+  isType.equal<true, never[], Actual>()
 })
 
 test('array type gets itself', () => {
   const s: string[] = ['a', 'b', 'c']
   type Actual = DropLast<typeof s>
-  assertType.isTrue(true as Equal<string[], Actual>)
+  isType.equal<true, string[], Actual>()
 })
