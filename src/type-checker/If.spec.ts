4512449dbc6ec77d079eaa2d1cfdac329d9dd1ea
unional <homawong@gmail.com>
2021-01-26 09:44:55 -0800

    fix: replace usage of If<> with tenary

--- src/type-checker/If.spec.ts
+++
@@ -1,9 +0,0 @@
-import { If, isType } from '..'
-
-test('true gets Then', () => {
-  isType.equal<true, 2, If<true, 2, 3>>()
-})
-
-test('false gets Else', () => {
-  isType.equal<true, 3, If<false, 2, 3>>()
-})
