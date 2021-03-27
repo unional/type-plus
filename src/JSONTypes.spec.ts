0425b43ef3d0fae573d8c1284af6e6098b9ea740
Homa Wong <homawong@gmail.com>
2021-03-27 13:49:46 -0700

    feat: add JSONTypes.get()
    and add JSONObject, JSONArray, and JSONPrimitive

--- src/JSONTypes.spec.ts
+++ src/JSONTypes.spec.ts
@@ -1,5 +1,6 @@
 import { assertType } from './assertion'
-import { JSONTypes } from './JSONTypes'
+import { JSONObject, JSONTypes } from './JSONTypes'
+import { isType } from './predicates'
 
 test('empty object', () => {
   assertType<JSONTypes>({})
@@ -11,4 +12,32 @@ test('empty array', () => {
 
 test('string array', () => {
   assertType<JSONTypes>(['a'])
+})
+
+test('JSONObject', () => {
+  isType<JSONObject>({})
+})
+
+describe('JSONTypes.get', () => {
+  test('cast to T | undefined without props', () => {
+    const a = JSONTypes.get<string>('abc')
+    isType<string | undefined>(a)
+    expect(a).toBe('abc')
+  })
+  test('get object props', () => {
+    const a = JSONTypes.get<string>({ a: { b: 'abc' } }, 'a', 'b')
+    isType<string | undefined>(a)
+    expect(a).toBe('abc')
+  })
+
+  test('get array entry', () => {
+    const a = JSONTypes.get<string>(['abc'], 0)
+    isType<string | undefined>(a)
+    expect(a).toBe('abc')
+  })
+  test('nested', () => {
+    const a = JSONTypes.get<string>({ a: { b: [{ c: 'abc' }] } }, 'a', 'b', 0, 'c')
+    isType<string | undefined>(a)
+    expect(a).toBe('abc')
+  })
 })
