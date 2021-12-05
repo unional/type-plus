7db816230075e515f3423e939890430438c907a5
Homa Wong <homawong@gmail.com>
2021-12-04 16:56:28 -0800

    fix: update to typescript 4.5

--- src/class/isConstructor.ts
+++ src/class/isConstructor.ts
@@ -4,8 +4,7 @@ export function isConstructor(subject: unknown): subject is AnyConstructor {
   try {
     new (subject as AnyConstructor)()
   }
-  catch (err) {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  catch (err: any) {
     const msg = err?.message as string | undefined
     if (msg && msg.indexOf('is not a constructor') >= 0) {
       return false
