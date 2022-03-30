6151e5968f442048a50a2ec458cd043cab1e0ea2
unional <homawong@gmail.com>
2022-03-29 23:54:17 -0700

    fix: CreateTuple

--- ts/array/CreateTuple.ts
+++ ts/array/CreateTuple.ts
@@ -1,12 +1,11 @@
-import type { IsPositive } from '../math'
+import { IsPositive } from '../math'
 
 /**
  * Creates `Tuple<T>` with `L` number of elements.
+ * @note updated implementation based on below
+ * @see https://github.com/microsoft/TypeScript/issues/47874#issuecomment-1039157322
  */
-export type CreateTuple<L extends number, T = any> =
-  IsPositive<L> extends true ? CreateTuple.Device<[], L, T> : never
-
-export namespace CreateTuple {
-  export type Device<R extends any[], L extends number, T> =
-    R['length'] extends L ? R : Device<[T, ...R], L, T>
-}
+export type CreateTuple<L extends number, T = any, A extends T[] = []> =
+  IsPositive<L> extends true
+  ? L extends A['length'] ? A : CreateTuple<L, T, [T, ...A]>
+  : never
