5975265581190616b94381f083bc3e0c5ed85b11
Homa Wong <homawong@gmail.com>
2022-04-12 23:28:25 -0700

    feat: GreaterThan supports negative and large numbers (#165)
    Thank you for the idea! @Jcparkyn 🍻

--- ts/math/GreaterThan.spec.ts
+++ ts/math/GreaterThan.spec.ts
@@ -1,10 +1,8 @@
 import { GreaterThan, isType } from '..'
 
-test('only support positive and whole number', () => {
+test('only support whole number', () => {
   isType.equal<true, never, GreaterThan<0.1, 1>>()
-  isType.equal<true, never, GreaterThan<-1, 1>>()
   isType.equal<true, never, GreaterThan<1, 0.1>>()
-  isType.equal<true, never, GreaterThan<1, -1>>()
 })
 
 test('number gets never', () => {
@@ -13,31 +11,65 @@ test('number gets never', () => {
 })
 
 test('override Fail case', () => {
-  isType.equal<true, number, GreaterThan<number, 1, number>>()
+  isType.equal<true, number, GreaterThan<number, 2, number>>()
 })
 
 test('n > n is false', () => {
-  isType.f<GreaterThan<0, 0>>()
-  isType.f<GreaterThan<1, 1>>()
-  isType.f<GreaterThan<12, 12>>()
+  isType.equal<true, false, GreaterThan<0, 0>>()
+  isType.equal<true, false, GreaterThan<1, 1>>()
+  isType.equal<true, false, GreaterThan<12, 12>>()
 })
 
 test('with same number of digits', () => {
-  isType.t<GreaterThan<1, 0>>()
-  isType.t<GreaterThan<22, 11>>()
-  isType.t<GreaterThan<20, 19>>()
-  isType.t<GreaterThan<19, 10>>()
+  isType.equal<true, true, GreaterThan<1, 0>>()
+  isType.equal<true, true, GreaterThan<22, 11>>()
+  isType.equal<true, true, GreaterThan<20, 19>>()
+  isType.equal<true, true, GreaterThan<19, 10>>()
 
-  isType.f<GreaterThan<0, 1>>()
-  isType.f<GreaterThan<11, 22>>()
-  isType.f<GreaterThan<19, 20>>()
-  isType.f<GreaterThan<10, 19>>()
+  isType.equal<true, false, GreaterThan<0, 1>>()
+  isType.equal<true, false, GreaterThan<11, 22>>()
+  isType.equal<true, false, GreaterThan<19, 20>>()
+  isType.equal<true, false, GreaterThan<10, 19>>()
 })
 
 test('with different number of digits', () => {
-  isType.f<GreaterThan<0, 1>>()
-  isType.f<GreaterThan<9, 100>>()
-  isType.t<GreaterThan<10, 1>>()
-  isType.t<GreaterThan<123, 32>>()
-  isType.t<GreaterThan<123, 13>>()
+  isType.equal<true, false, GreaterThan<0, 1>>()
+  isType.equal<true, false, GreaterThan<9, 100>>()
+  isType.equal<true, true, GreaterThan<10, 1>>()
+  isType.equal<true, true, GreaterThan<123, 32>>()
+  isType.equal<true, true, GreaterThan<123, 13>>()
+})
+
+test('-0 > 0 and 0 > -0 are false', () => {
+  isType.equal<true, false, GreaterThan<-0, 0>>()
+  isType.equal<true, false, GreaterThan<0, -0>>()
+})
+
+test('works with negative numbers', () => {
+  isType.equal<true, true, GreaterThan<0, -1>>()
+  isType.equal<true, true, GreaterThan<1, -1>>()
+  isType.equal<true, true, GreaterThan<2, -1>>()
+
+  isType.equal<true, false, GreaterThan<-1, 0>>()
+  isType.equal<true, false, GreaterThan<-1, 1>>()
+  isType.equal<true, false, GreaterThan<-1, 2>>()
+
+  isType.equal<true, false, GreaterThan<-0, -0>>()
+  isType.equal<true, false, GreaterThan<-1, -1>>()
+
+  isType.equal<true, true, GreaterThan<-0, -1>>()
+  isType.equal<true, false, GreaterThan<-1, -1>>()
+  isType.equal<true, false, GreaterThan<-2, -1>>()
+
+  isType.equal<true, false, GreaterThan<-1, -0>>()
+  isType.equal<true, false, GreaterThan<-1, -1>>()
+  isType.equal<true, true, GreaterThan<-1, -2>>()
+})
+
+test('work with large numbers', () => {
+  isType.equal<true, true, GreaterThan<1000000, 0>>()
+  isType.equal<true, false, GreaterThan<0, 1000000>>()
+
+  isType.equal<true, false, GreaterThan<-1000000, 0>>()
+  isType.equal<true, true, GreaterThan<0, -1000000>>()
 })
--- ts/math/GreaterThan.ts
+++ ts/math/GreaterThan.ts
@@ -1,28 +1,27 @@
 import type { Head, PadLeft, Tail } from '../array'
-import type { And, Equal } from '../predicates'
+import type { And, Equal, Or, Xor } from '../predicates'
 import type { Digit, DigitArray } from './Digit'
 import type { IsPositive } from './IsPositive'
 import type { IsWhole } from './IsWhole'
 import type { Max } from './Max'
 
 export type GreaterThan<A extends number, B extends number, Fail = never> =
-  And<
-    And<IsPositive<A>, IsWhole<A>>,
-    And<IsPositive<B>, IsWhole<B>>
-  > extends false ? Fail :
+  And<IsWhole<A>, IsWhole<B>> extends false ? Fail :
   number extends A ? Fail :
   number extends B ? Fail :
   Equal<A, B> extends true ? false :
-  GreaterThan.OnPositiveWhole<A, B>
+  Xor<IsPositive<A>, IsPositive<B>> extends true ? IsPositive<A> :
+  Or<IsPositive<A>, IsPositive<B>> extends false ? GreaterThan.ForWholeNumber<B, A> :
+  GreaterThan.ForWholeNumber<A, B>
 
 export namespace GreaterThan {
-  export type OnPositiveWhole<A extends number, B extends number> = (
-    DigitArray.FromNumber<A> extends infer DA ? DA extends number[] ?
-    DigitArray.FromNumber<B> extends infer DB ? DB extends number[] ?
+  export type ForWholeNumber<A extends number, B extends number> = (
+    DigitArray.FromNumberAbs<A> extends infer DA ? DA extends number[] ?
+    DigitArray.FromNumberAbs<B> extends infer DB ? DB extends number[] ?
     Max<DA['length'], DB['length']> extends infer M ? M extends number ?
     PadLeft<DA, M, 0> extends infer PDA ? PDA extends number[] ?
     PadLeft<DB, M, 0> extends infer PDB ? PDB extends number[] ?
-    OnDigitArray<PDA, PDB>
+    ForDigitArray<PDA, PDB>
     : never : never
     : never : never
     : never : never
@@ -30,9 +29,9 @@ export namespace GreaterThan {
     : never : never
   )
 
-  export type OnDigitArray<DA extends number[], DB extends number[]> = (
+  export type ForDigitArray<DA extends number[], DB extends number[]> = (
     Equal<Head<DA>, Head<DB>> extends true
-    ? OnDigitArray<Tail<DA>, Tail<DB>>
+    ? ForDigitArray<Tail<DA>, Tail<DB>>
     : Digit.GreaterThan<Head<DA>, Head<DB>>
   )
 }
