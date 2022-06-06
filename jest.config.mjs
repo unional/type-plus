784f5a554e3ac8db594e69d263ddf285141ceaea
Homa Wong <homawong@gmail.com>
2022-06-05 20:29:26 -0700

    chore: add comments

--- jest.config.mjs
+++ jest.config.mjs
@@ -13,7 +13,10 @@ export default {
     '!<rootDir>/ts/types/*'
   ],
   moduleNameMapper: {
+    // remove the phantom `.js` extension
     '^(\\.{1,2}/.*)\\.js$': '$1',
+    // If dependency doing `import ... from '#<pkg>'.
+    // e.g. `chalk` has this: `import ... form '#ansi-styles'`
     '#(.*)': '<rootDir>/node_modules/$1'
   },
   modulePathIgnorePatterns: [
@@ -28,9 +31,12 @@ export default {
     '**/?(*.)+(spec|test|integrate|accept|system|unit).[jt]s?(x)',
   ],
   transformIgnorePatterns: [
+    // Need to MANUALLY identify each ESM package, one by one
     'node_modules/(?!(@unional\\fixture|chalk)/)'
   ],
   transform: {
+    // use `babel-jest` to transpile js files
+    // can probably improve this by just doing `.js|mjs` in the `node_modules` folder.
     '^.+\\.(js|jsx|mjs)$': 'babel-jest',
   },
   watchPlugins: [
