b99d6f46cf12d985106ff37e6c1e2c1fb892021c
Homa Wong <homawong@gmail.com>
2022-06-11 21:43:28 -0700

    chore: disable body-max-line-length

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
+    'body-max-line-length': [0],
     'subject-case': [2, 'never', ['start-case', 'pascal-case']]
   }
 }
