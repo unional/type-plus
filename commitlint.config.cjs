7a2fcf7fa3ba012cb057e45dc73534e810255df5
Homa Wong <homawong@gmail.com>
2022-07-20 19:22:19 -0700

    chore: disable footer-max-line-length

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -2,6 +2,7 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'body-max-line-length': [0],
+    'footer-max-line-length': [0],
     'subject-case': [2, 'never', ['start-case', 'pascal-case']]
   }
 }
