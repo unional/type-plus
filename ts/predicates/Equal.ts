17150e8727f81c04489349e08f8d73b2910de6f3
Homa Wong <homawong@gmail.com>
2022-09-12 12:08:40 -0700

    fix: Equal<A,B> with union types
    The original implementation from `tyeppark` does not work with union type.

--- ts/predicates/Equal.ts
+++ ts/predicates/Equal.ts
@@ -1,14 +1,23 @@
 /**
+ * `<T>() => T extends A ? 1 : 2` idea originate from `typepark`
+ * But it does not work with union types.
+ *
+ * `<T>() => T extends A` is a trick to create an inferred type `T`.
+ * This is needed for `boolean`, `string`, and `number`,
+ * as they supports literal types.
+ */
+
+/**
  * Checks if two types are equal.
- * Borrow from `typepark`.
- * The simple `A extends B ? B extends A ? true : false : false`
- * does not work with boolean type.
  */
 export type Equal<A, B, Then = true, Else = false> =
-  (<T>() => T extends A ? 1 : 2) extends (<T>() => T extends B ? 1 : 2)
-  ? Then : Else
+  [A, B] extends [object, object]
+  ? (A extends B ? B extends A ? Then : Else : Else)
+  : (<T>() => T extends A ? 1 : 2) extends (<T>() => T extends B ? 1 : 2) ? Then : Else
 export type IsEqual<A, B> = Equal<A, B>
 
 export type NotEqual<A, B, Then = true, Else = false> =
-  (<T>() => T extends A ? 1 : 2) extends (<T>() => T extends B ? 1 : 2) ? Else : Then
+  [A, B] extends [object, object]
+  ? (A extends B ? B extends A ? Else : Then : Then)
+  : (<T>() => T extends A ? 1 : 2) extends (<T>() => T extends B ? 1 : 2) ? Else : Then
 export type IsNotEqual<A, B> = NotEqual<A, B>
