9637d1ef1c47e896169aa2e73acdf4bb0ae11d3b
Homa Wong <homawong@gmail.com>
2022-09-15 10:30:18 -0700

    fix: add `EitherOrBoth`
    deprecate `EitherAnd`

--- ts/utils/index.spec.ts
+++ ts/utils/index.spec.ts
@@ -1,4 +1,4 @@
-import { CanAssign, EitherAnd, isType } from '../index.js'
+import { CanAssign, EitherOrBoth, isType } from '../index.js'
 
 describe('Either<A, B>', () => {
   type A = {
@@ -18,21 +18,21 @@ describe('Either<A, B>', () => {
     z?: number
   }
   it('allows just A', () => {
-    type S = EitherAnd<A, B>
+    type S = EitherOrBoth<A, B>
     isType.equal<true, true, CanAssign<{ a: number }, S>>()
     isType.equal<true, true, CanAssign<{ a: number, x?: string }, S>>()
     isType.equal<true, true, CanAssign<{ a: number, x: string }, S>>()
     isType.equal<true, false, CanAssign<{ a: number, x: number }, S>>()
   })
   it('allows just B', () => {
-    type S = EitherAnd<A, B>
+    type S = EitherOrBoth<A, B>
     isType.equal<true, true, CanAssign<{ b: number }, S>>()
     isType.equal<true, true, CanAssign<{ b: number, x?: boolean }, S>>()
     isType.equal<true, true, CanAssign<{ b: number, x: boolean }, S>>()
     isType.equal<true, false, CanAssign<{ b: number, x: number }, S>>()
   })
   it('allows A & B', () => {
-    type S = EitherAnd<A, B>
+    type S = EitherOrBoth<A, B>
     isType.equal<true, true, CanAssign<{ a: number, b: number }, S>>()
     isType.equal<true, true, CanAssign<{ a: number, b: number, x?: boolean }, S>>()
     isType.equal<true, true, CanAssign<{ a: number, b: number, x: boolean }, S>>()
@@ -43,7 +43,7 @@ describe('Either<A, B>', () => {
     isType.equal<true, false, CanAssign<{ a: number, b: number, x: number }, S>>()
   })
   it('can use isType() to type guard the value', () => {
-    type S = EitherAnd<A, B>
+    type S = EitherOrBoth<A, B>
     function foo(o: S) {
       if (isType<A>(o, o => typeof o.a === 'number')) {
         isType.equal<true, A, typeof o>()
@@ -55,13 +55,13 @@ describe('Either<A, B>', () => {
     foo({ a: 1 })
   })
   it('will make sure required field to be filled in when specifying optional field', () => {
-    type S = EitherAnd<A, C>
+    type S = EitherOrBoth<A, C>
 
     isType.equal<true, false, CanAssign<{ x: 'x' }, S>>()
     isType.equal<true, true, CanAssign<{ a: 1, x: 'x' }, S>>()
   })
   it('allows A, B, C', () => {
-    type S = EitherAnd<A, B, C>
+    type S = EitherOrBoth<A, B, C>
     isType.equal<true, true, CanAssign<{ a: number }, S>>()
     isType.equal<true, true, CanAssign<{ b: number }, S>>()
     isType.equal<true, true, CanAssign<{ c: number }, S>>()
@@ -72,7 +72,7 @@ describe('Either<A, B>', () => {
     isType.equal<true, true, CanAssign<{ a: number, b: number, c: number }, S>>()
   })
   it('allows A, B, C, D', () => {
-    type S = EitherAnd<A, B, C, D>
+    type S = EitherOrBoth<A, B, C, D>
     isType.equal<true, true, CanAssign<{ a: number }, S>>()
     isType.equal<true, true, CanAssign<{ b: number }, S>>()
     isType.equal<true, true, CanAssign<{ c: number }, S>>()
@@ -97,36 +97,36 @@ describe('Either<A, B>', () => {
     //  A | B | (A & B) | C | ((A | B | (A & B)) & C)
     //= A | B | C | (A & B) | ((A | B | (A & B)) & C)
     //= A | B | C | (A & B) | (A & C) | (B & C) | (A & B & C)
-    type AB_C = EitherAnd<EitherAnd<A, B>, C>
+    type AB_C = EitherOrBoth<EitherOrBoth<A, B>, C>
     // A | B | C | (B & C) | (A & (B | C | (B & C)))
     //= A | B | C | (B & C) | (A & (B | C | (B & C)))
     //= A | B | C | (B & C) | (A & B) | (A & C) | (A & B & C)
     //= A | B | C | (A & B) | (A & C) | (B & C) | (A & B & C)
-    type A_BC = EitherAnd<A, EitherAnd<B, C>>
+    type A_BC = EitherOrBoth<A, EitherOrBoth<B, C>>
 
     // A | B | (A & B) | C | (A & C) | (B & C) | (A & B & C)
     //= A | B | C | (A & B) | (A & C) | (B & C) | (A & B & C)
-    type ABC = EitherAnd<A, B, C>
+    type ABC = EitherOrBoth<A, B, C>
 
     // A | B | (A & B) | C | D | (C & D) | ((A | B | (A & B)) & (C | D | (C & D)))
     //= A | B | C | D | (A & B) | (C & D) | ((A | B | (A & B)) & (C | D | (C & D)))
     //= A | B | C | D | (A & B) | (C & D) | (((A | B | (A & B)) & C) | ((A | B | (A & B)) & D) | ((A | B | (A & B)) & (C & D))
     //= A | B | C | D | (A & B) | (C & D) | (A & C) | (B & C) | (A & B & C) | (A & D) | (B & D) | (A & B & D) | (A & C & D) | (B & C & D) | (A & B & C & D)
     //= A | B | C | D | (A & B) | (A & C) | (A & D) | (B & C) | (B & D) | (C & D) | (A & B & C) | (A & B & D) | (A & C & D) | (B & C & D) | (A & B & C & D)
-    type AB_CD = EitherAnd<EitherAnd<A, B>, EitherAnd<C, D>>
+    type AB_CD = EitherOrBoth<EitherOrBoth<A, B>, EitherOrBoth<C, D>>
 
     // A | B | C | D | (B & C) | (B & D) | (C & D) | (B & C & D) | (A & (B | C | D | (B & C) | (B & D) | (C & D) | (B & C & D)))
     //= A | B | C | D | (B & C) | (B & D) | (C & D) | (B & C & D) | (A & B) | (A & C) | (A & D) | (A & B & C) | (A & B & D) | (A & C & D) | (A & B & C & D)
     //= A | B | C | D | (A & B) | (A & C) | (A & D) | (B & C) | (B & D) | (C & D) | (A & B & C) | (A & B & D) | (A & C & D) | (B & C & D) | (A & B & C & D)
-    type A_BCD = EitherAnd<A, EitherAnd<B, C, D>>
+    type A_BCD = EitherOrBoth<A, EitherOrBoth<B, C, D>>
 
     // A | B | (A & B) | C | D | (A & C) | (B & C) | (A & B & C) | ((A | B | (A & B) | C | (A & C) | (B & C) | (A & B & C)) & D)
     //= A | B | (A & B) | C | D | (A & C) | (B & C) | (A & B & C) | (A & D) | (B & D) | (A & B & D) | (C & D) | (A & C & D) | (B & C & D) | (A & B & C & D)
     //= A | B | C | D | (A & B) | (A & C) | (A & D) | (B & C) | (B & D) | (C & D) | (A & B & C) | (A & B & D) | (A & C & D) | (B & C & D) | (A & B & C & D)
-    type ABC_D = EitherAnd<EitherAnd<A, B, C>, D>
+    type ABC_D = EitherOrBoth<EitherOrBoth<A, B, C>, D>
 
     //  A | B | C | D | (A & B) | (A & C) | (A & D) | (B & C) | (B & D) | (C & D) | (A & B & C) | (A & B & D) | (A & C & D) | (B & C & D) | (A & B & C & D)
-    type ABCD = EitherAnd<A, B, C, D>
+    type ABCD = EitherOrBoth<A, B, C, D>
 
     isType.equal<true, ABC, A_BC>()
     isType.equal<true, ABC, AB_C>()
--- ts/utils/index.ts
+++ ts/utils/index.ts
@@ -20,7 +20,7 @@ export type { Widen } from './Widen.js'
  * config({ src: 'src', minify: false })
  * config({ minify: false })  // INVALID
  */
-export type EitherAnd<A, B, C = void, D = void> = C extends void
+export type EitherOrBoth<A, B, C = void, D = void> = C extends void
   ? A | B | A & B
   : (D extends void
     ? A | B | C | A & B | A & C | B & C | A & B & C
@@ -29,3 +29,23 @@ export type EitherAnd<A, B, C = void, D = void> = C extends void
     A & B & C | A & B & D | A & C & D | B & C & D |
     A & B & C & D
   )
+
+/**
+ * `A` | `B` | `A & B`.
+ * Supports up to 4 types, and it is composable.
+ * This is useful when you want to compose multiple options together,
+ * while preserving their required field and structure.
+ * If the types overlaps each other, you may run into some corner cases.
+ * But in general usage it should work as expected.
+ * @deprecated renamed to `EitherOrBoth`
+ * @example
+ * type A = { src: string, minify?: boolean }
+ * type B = { logLevel: number }
+ * function config(options: EitherAnd<A, B>) { }
+ *
+ * config({ logLevel: 1 })
+ * config({ src: 'src' })
+ * config({ src: 'src', minify: false })
+ * config({ minify: false })  // INVALID
+ */
+ export type EitherAnd<A, B, C = void, D = void> = EitherOrBoth<A, B, C, D>
