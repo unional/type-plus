036f837bf7db8beaa32de90880bb7f14c22dd535
Homa Wong <homawong@gmail.com>
2022-09-14 21:39:44 -0700

    docs: add JSDocs

--- ts/utils/index.ts
+++ ts/utils/index.ts
@@ -3,8 +3,23 @@ export type { NonNull } from './NonNull.js'
 export type { NonUndefined } from './NonUndefined.js'
 export type { Widen } from './Widen.js'
 
-
-
+/**
+ * `A` | `B` | `A & B`.
+ * Supports up to 4 types, and it is composable.
+ * This is useful when you want to compose multiple options together,
+ * while preserving their required field and structure.
+ * If the types overlaps each other, you may run into some corner cases.
+ * But in general usage it should work as expected.
+ * @example
+ * type A = { src: string, minify?: boolean }
+ * type B = { logLevel: number }
+ * function config(options: EitherAnd<A, B>) { }
+ *
+ * config({ logLevel: 1 })
+ * config({ src: 'src' })
+ * config({ src: 'src', minify: false })
+ * config({ minify: false })  // INVALID
+ */
 export type EitherAnd<A, B, C = void, D = void> = C extends void
   ? A | B | A & B
   : (D extends void
