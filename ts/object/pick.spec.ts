10adab495f2026b3d7f06f94ae91fcc22dcd1398
Homa Wong <homawong@gmail.com>
2022-10-12 17:18:09 -0700

    fix: improve pick and omit
    to support any number of keys

--- ts/object/pick.spec.ts
+++ ts/object/pick.spec.ts
@@ -1,66 +1,70 @@
 import { assertType, canAssign, Equal, Pick, pick } from '../index.js'
 
-test('pick properties from object', () => {
-  const actual = pick({ a: 1, b: 2 }, 'a')
+describe(`${pick.name}()`, () => {
+  it('picks properties from object', () => {
+    const actual = pick({ a: 1, b: 2 }, 'a')
 
-  expect(actual).toEqual({ a: 1 })
+    expect(actual).toEqual({ a: 1 })
+  })
 })
 
-test('distributive pick', () => {
-  type Action = InvokeAction | ReturnAction
+describe(`Pick<T, K>`, () => {
+  test('distributive pick', () => {
+    type Action = InvokeAction | ReturnAction
 
-  type InvokeAction = {
-    type: 'invoke',
-    id: string,
-    payload: string[],
-  }
+    type InvokeAction = {
+      type: 'invoke',
+      id: string,
+      payload: string[],
+    }
 
-  type ReturnAction = {
-    type: 'return',
-    id: string,
-    payload: string,
-  }
+    type ReturnAction = {
+      type: 'return',
+      id: string,
+      payload: string,
+    }
 
-  const x: Pick<Action, 'type' | 'payload'> = { type: 'invoke', payload: [] }
+    const x: Pick<Action, 'type' | 'payload'> = { type: 'invoke', payload: [] }
 
-  const actions: Action[] = []
+    const actions: Action[] = []
 
-  actions.push({ ...x, id: '1' })
-})
+    actions.push({ ...x, id: '1' })
+  })
 
-test('distributive pick with disjoined keys', () => {
-  type Union = {
-    type: 'A',
-    foo: string,
-  } | {
-    type: 'B',
-    foo: string,
-    bar: string,
-  }
-  type Id<T> = { [P in keyof T]: T[P] }
-  let x: Id<Pick<Union, 'type' | 'bar'>> = { type: 'A' }
-  x = { type: 'B', bar: 'bar' }
+  test('distributive pick with disjoined keys', () => {
+    type Union = {
+      type: 'A',
+      foo: string,
+    } | {
+      type: 'B',
+      foo: string,
+      bar: string,
+    }
+    type Id<T> = { [P in keyof T]: T[P] }
+    let x: Id<Pick<Union, 'type' | 'bar'>> = { type: 'A' }
+    x = { type: 'B', bar: 'bar' }
 
-  expect(x.bar).toBe('bar')
-})
+    expect(x.bar).toBe('bar')
+  })
 
-test('intersection types with generic', () => {
-  type Foo = { a: string, b: string }
-  function foo<T>(input: Pick<Foo & T, 'a'>): void {
-    assertType.isString(input.a)
-  }
-  foo({ a: '1' })
-})
+  test('intersection types with generic', () => {
+    type Foo = { a: string, b: string }
+    function foo<T>(input: Pick<Foo & T, 'a'>): void {
+      assertType.isString(input.a)
+    }
+    foo({ a: '1' })
+  })
 
-test('optional property remains optional', () => {
-  type Foo = { a?: string, b: string }
-  type A = Pick<Foo, 'a'>
-  assertType.isTrue(canAssign<A>()({}))
-})
+  test('optional property remains optional', () => {
+    type Foo = { a?: string, b: string }
+    type A = Pick<Foo, 'a'>
+    assertType.isTrue(canAssign<A>()({}))
+  })
 
-test('pick never gets empty object', () => {
-  type A = { a: number }
-  type S = Pick<A, never>
-  type K = keyof S
-  assertType.isTrue(true as Equal<K, never>)
+  test('pick never gets empty object', () => {
+    type A = { a: number }
+    type S = Pick<A, never>
+    type K = keyof S
+    assertType.isTrue(true as Equal<K, never>)
+  })
 })
--- ts/object/pick.ts
+++ ts/object/pick.ts
@@ -1,3 +1,4 @@
+import { record } from 'type-plus'
 import { UnionKeys } from '../UnionKeys.js'
 import type { AnyRecord } from './AnyRecord.js'
 import { reduceByKey } from './reduceKey.js'
@@ -14,11 +15,12 @@ export function pick<T extends AnyRecord, P1 extends UnionKeys<T>, P2 extends Un
 export function pick<T extends AnyRecord, P1 extends UnionKeys<T>, P2 extends UnionKeys<T>, P3 extends UnionKeys<T>, P4 extends UnionKeys<T>, P5 extends UnionKeys<T>, P6 extends UnionKeys<T>, P7 extends UnionKeys<T>, P8 extends UnionKeys<T>, P9 extends UnionKeys<T>, P10 extends UnionKeys<T>>(subject: T, prop1: P1, prop2: P2, prop3: P3, prop4: P4, prop5: P5, prop6: P6, prop7: P7, prop8: P8, prop9: P9, prop10: P10): Pick<T, P1 | P2 | P3 | P4 | P5 | P6 | P7 | P8 | P9 | P10>
 export function pick<T extends AnyRecord, P1 extends UnionKeys<T>, P2 extends UnionKeys<T>, P3 extends UnionKeys<T>, P4 extends UnionKeys<T>, P5 extends UnionKeys<T>, P6 extends UnionKeys<T>, P7 extends UnionKeys<T>, P8 extends UnionKeys<T>, P9 extends UnionKeys<T>, P10 extends UnionKeys<T>, P11 extends UnionKeys<T>>(subject: T, prop1: P1, prop2: P2, prop3: P3, prop4: P4, prop5: P5, prop6: P6, prop7: P7, prop8: P8, prop9: P9, prop10: P10, prop11: P11): Pick<T, P1 | P2 | P3 | P4 | P5 | P6 | P7 | P8 | P9 | P10 | P11>
 export function pick<T extends AnyRecord, P1 extends UnionKeys<T>, P2 extends UnionKeys<T>, P3 extends UnionKeys<T>, P4 extends UnionKeys<T>, P5 extends UnionKeys<T>, P6 extends UnionKeys<T>, P7 extends UnionKeys<T>, P8 extends UnionKeys<T>, P9 extends UnionKeys<T>, P10 extends UnionKeys<T>, P11 extends UnionKeys<T>, P12 extends UnionKeys<T>>(subject: T, prop1: P1, prop2: P2, prop3: P3, prop4: P4, prop5: P5, prop6: P6, prop7: P7, prop8: P8, prop9: P9, prop10: P10, prop11: P11, prop12: P12): Pick<T, P1 | P2 | P3 | P4 | P5 | P6 | P7 | P8 | P9 | P10 | P11 | P12>
+export function pick<T extends AnyRecord, Props extends UnionKeys<T>>(subject: T, ...props: Props[]): Pick<T, Props>
 export function pick<T extends AnyRecord>(subject: T, ...props: Array<UnionKeys<T>>) {
   return reduceByKey(subject, (p, k) => {
     if (props.indexOf(k) >= 0) p[k] = subject[k]
     return p
-  }, {} as T)
+  }, record())
 }
 
 /**
