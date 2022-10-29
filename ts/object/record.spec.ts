7cf5f68b30238d3a47d2cbbec140a9079696ffcb
Homa Wong <homawong@gmail.com>
2022-10-29 14:24:28 -0700

    test: update tests

--- ts/object/record.spec.ts
+++ ts/object/record.spec.ts
@@ -1,62 +1,64 @@
 import { isType, KeyTypes, record } from '../index.js'
 
-test('create an empty record with value default to unknown', () => {
-  const a = record()
-  a[1] = 2
-  a['a'] = 'b'
-  a[Symbol()] = true
-  isType.equal<true, Record<KeyTypes, unknown>, typeof a>()
-})
+describe(`${record.name}()`, () => {
+  it('creates an empty record with value default to unknown', () => {
+    const a = record()
+    a[1] = 2
+    a['a'] = 'b'
+    a[Symbol()] = true
+    isType.equal<true, Record<KeyTypes, unknown>, typeof a>()
+  })
 
-test('specify key and value types', () => {
-  const a = record<string, number>()
-  // even though the key type is string,
-  // by default JavaScript will cast number index to string
-  // so TypeScript also allows it.
-  a[1] = 2
-  a['a'] = 3
-  isType.equal<true, Record<string, number>, typeof a>()
-})
+  it('can specify key and value types', () => {
+    const a = record<string, number>()
+    // even though the key type is string,
+    // by default JavaScript will cast number index to string
+    // so TypeScript also allows it.
+    a[1] = 2
+    a['a'] = 3
+    isType.equal<true, Record<string, number>, typeof a>()
+  })
 
-test('no prototype', () => {
-  const a = record()
-  expect(Object.getPrototypeOf(a)).toBeNull()
-})
+  it('has no prototype', () => {
+    const a = record()
+    expect(Object.getPrototypeOf(a)).toBeNull()
+  })
 
-test('infer type from initial value, the key type is widen', () => {
-  const stringRecord = record({ a: 1 })
-  isType.equal<true, Record<string, number>, typeof stringRecord>()
+  it('has no prototype with initial value', () => {
+    const a = record({ a: 1 })
+    expect(Object.getPrototypeOf(a)).toBeNull()
+  })
 
-  const numberRecord = record({ 1: 3 })
-  isType.equal<true, Record<number, number>, typeof numberRecord>()
+  it('infers type from initial value, the key type is widen', () => {
+    const stringRecord = record({ a: 1 })
+    isType.equal<true, Record<string, number>, typeof stringRecord>()
 
-  const stringConstRecord = record({ a: 1 } as const)
-  isType.equal<true, Record<string, 1>, typeof stringConstRecord>()
+    const numberRecord = record({ 1: 3 })
+    isType.equal<true, Record<number, number>, typeof numberRecord>()
 
-  const stringConstRecord2 = record({ a: 1 as const })
-  isType.equal<true, Record<string, 1>, typeof stringConstRecord2>()
+    const stringConstRecord = record({ a: 1 } as const)
+    isType.equal<true, Record<string, 1>, typeof stringConstRecord>()
 
-  const stringConstRecord3 = record({ a: 1 as const, b: 'b' })
-  isType.equal<true, Record<string, 1 | string>, typeof stringConstRecord3>()
+    const stringConstRecord2 = record({ a: 1 as const })
+    isType.equal<true, Record<string, 1>, typeof stringConstRecord2>()
 
-  const stringConstRecord4 = record({ a: 1 as const, b: 'b' as const })
-  isType.equal<true, Record<string, 1 | 'b'>, typeof stringConstRecord4>()
-})
+    const stringConstRecord3 = record({ a: 1 as const, b: 'b' })
+    isType.equal<true, Record<string, 1 | string>, typeof stringConstRecord3>()
 
-test('no prototype with initial value', () => {
-  const a = record({ a: 1 })
-  expect(Object.getPrototypeOf(a)).toBeNull()
-})
+    const stringConstRecord4 = record({ a: 1 as const, b: 'b' as const })
+    isType.equal<true, Record<string, 1 | 'b'>, typeof stringConstRecord4>()
+  })
 
-test('Object.keys() gets the keys of the initial value', () => {
-  const a = record({ a: 1, b: 2 })
-  expect(Object.keys(a)).toEqual(['a', 'b'])
-})
+  it('has the keys of the initial value', () => {
+    const a = record({ a: 1, b: 2 })
+    expect(Object.keys(a)).toEqual(['a', 'b'])
+  })
 
-it('can specify a custom record', () => {
-  const a = record<{ a: number }>()
-  isType.equal<true, { a: number }, typeof a>()
+  it('can specify a custom record', () => {
+    const a = record<{ a: number }>()
+    isType.equal<true, { a: number }, typeof a>()
 
-  const b = record<{ b: string }>({ b: 'b' })
-  isType.equal<true, { b: string }, typeof b>()
+    const b = record<{ b: string }>({ b: 'b' })
+    isType.equal<true, { b: string }, typeof b>()
+  })
 })
