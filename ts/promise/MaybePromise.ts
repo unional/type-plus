f13f1a712f22f824c6a5060608c89bcaf48d3476
Homa Wong <homawong@gmail.com>
2022-10-30 19:20:28 -0700

    refactor: rename to MaybePromise

--- ts/promise/MaybePromise.ts
+++ ts/promise/MaybePromise.ts
@@ -3,14 +3,14 @@ import { isPromise } from './isPromise.js'
 /**
  * `T | Promise<T>`
  */
-export type PromiseOrValue<T> = T | Promise<T>
+export type MaybePromise<T> = T | Promise<T>
 
 /**
  * Transforms the value within the promise.
  *
  * @return a new promise with the transformed result.
  */
-export function transformPromiseOrValue<T, R>(value: Promise<T>, transformer: (value: T) => R): Promise<R>
+export function transformMaybePromise<T, R>(value: Promise<T>, transformer: (value: T) => R): Promise<R>
 /**
  * Transforms the value, or if the value is a promise,
  * transform the resolved value of the promise.
@@ -18,11 +18,11 @@ export function transformPromiseOrValue<T, R>(value: Promise<T>, transformer: (v
  * @return the transformed result,
  * or if the value is a promise, a new promse with the transformed result.
  */
-export function transformPromiseOrValue<T, R>(value: T, transformer: (value: T) => R): T extends Promise<any> ? Promise<R> : R
-export function transformPromiseOrValue<T, R>(value: PromiseOrValue<T>, transformer: (value: T) => R) {
+export function transformMaybePromise<T, R>(value: T, transformer: (value: T) => R): T extends Promise<any> ? Promise<R> : R
+export function transformMaybePromise<T, R>(value: MaybePromise<T>, transformer: (value: T) => R) {
   return isPromise(value) ? value.then(transformer) : transformer(value)
 }
 
-export const PromiseOrValue = {
-  transform: transformPromiseOrValue
+export const MaybePromise = {
+  transform: transformMaybePromise
 }
