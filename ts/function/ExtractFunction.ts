459bd1b9c8af04d37d6ea88222f7402bbeb870c2
unional <homawong@gmail.com>
2022-11-19 23:12:16 -0800

    chore: add note about function override

--- ts/function/ExtractFunction.ts
+++ ts/function/ExtractFunction.ts
@@ -2,6 +2,8 @@ import { AnyFunction } from './AnyFunction.js'
 
 /**
  * Extract the function signature from a composit type T.
+ *
+ * @note does not work with function overloads.
  */
 export type ExtractFunction<T extends AnyFunction> = T extends AnyFunction<infer P, infer R>
   ? (...args: P) => R : never
