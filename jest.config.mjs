f395edbcc00d28446752f0588cd21774704136f1
unional <homawong@gmail.com>
2022-12-09 01:26:37 -0800

    chore: add back ignore of type/type-checker folder during test

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('jest').Config} */
 export default {
   preset: '@repobuddy/jest/presets/ts-esm',
+  modulePathIgnorePatterns: [
+    '<rootDir>/ts/types',
+    '<rootDir>/ts/type-checker'
+  ]
 }
