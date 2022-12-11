d297b7b2ad8c375cb336ede6f51f8411ddc9d447
unional <homawong@gmail.com>
2022-12-10 20:00:35 -0800

    chore: up @repobuddy/jest

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('jest').Config} */
 export default {
-  preset: '@repobuddy/jest/presets/ts-esm',
+  preset: '@repobuddy/jest/presets/ts',
   modulePathIgnorePatterns: [
     '<rootDir>/ts/types',
     '<rootDir>/ts/type-checker'
