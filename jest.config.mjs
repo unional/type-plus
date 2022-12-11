585c2a4aecad25778453376ca12dfb7976fbfa93
unional <homawong@gmail.com>
2022-12-10 20:11:14 -0800

    chore: update to use watch

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('jest').Config} */
 export default {
-  preset: '@repobuddy/jest/presets/ts',
+  preset: '@repobuddy/jest/presets/ts-esm-watch',
   modulePathIgnorePatterns: [
     '<rootDir>/ts/types',
     '<rootDir>/ts/type-checker'
