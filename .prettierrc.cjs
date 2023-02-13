c45a6c16592f2a813964eefaf37af65ea96f215f
unional <homawong@gmail.com>
2023-02-13 15:59:29 -0800

    chore: add why to ignore

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,11 +1,11 @@
+/** @type {import('prettier').Config} */
 module.exports = {
-  editorconfig: true,
-  printWidth: 110,
-  endOfLine: 'auto',
-  semi: false,
-  singleQuote: true,
-
-  // For ES5, trailing commas cannot be used in function parameters; it is counterintuitive
-  // to use them for arrays only
-  trailingComma: 'none'
-};
+	arrowParens: 'avoid',
+	printWidth: 110,
+	semi: false,
+	singleQuote: true,
+	// For ES5, trailing commas cannot be used in function parameters; it is counterintuitive
+	// to use them for arrays only
+	trailingComma: 'none',
+	useTabs: true
+}
