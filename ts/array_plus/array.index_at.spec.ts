8ac99f19e8df8f88376310eef2cca87a2bc742dd
unional <homawong@gmail.com>
2023-03-17 23:37:14 -0700

    fix: adjust to never

--- ts/array_plus/array.index_at.spec.ts
+++ ts/array_plus/array.index_at.spec.ts
@@ -1,6 +1,24 @@
 import { isType } from '../index.js'
 import type { IndexAt } from './array.index_at.js'
 
+it('returns never for empty tuple', () => {
+	isType.equal<true, IndexAt<[], 0>, never>()
+	isType.equal<true, IndexAt<[], 1>, never>()
+	isType.equal<true, IndexAt<[], -1>, never>()
+})
+
+it('returns N if A is an array and N is positive', () => {
+	isType.equal<true, IndexAt<string[], 0>, 0>()
+	isType.equal<true, IndexAt<string[], 1>, 1>()
+	isType.equal<true, IndexAt<string[], 100>, 100>()
+})
+
+it('returns N if A is an array and N is negative', () => {
+	isType.equal<true, IndexAt<string[], -0>, 0>()
+	isType.equal<true, IndexAt<string[], -1>, never>()
+	isType.equal<true, IndexAt<string[], -100>, never>()
+})
+
 it('returns type of the element for positive indexes', () => {
 	isType.equal<true, IndexAt<[1, 2, 3], 0>, 0>()
 	isType.equal<true, IndexAt<[1, 2, 3], 1>, 1>()
@@ -15,8 +33,8 @@ it('returns type of the element for negative indexes', () => {
 })
 
 it('returns undefined when index is out of range', () => {
-	isType.equal<true, IndexAt<[1, 2, 3], 3>, undefined>()
-	isType.equal<true, IndexAt<[1, 2, 3], -4>, undefined>()
+	isType.equal<true, IndexAt<[1, 2, 3], 3>, never>()
+	isType.equal<true, IndexAt<[1, 2, 3], -4>, never>()
 })
 
 it('returns never if N is not an integer', () => {
--- ts/array_plus/array.index_at.ts
+++ ts/array_plus/array.index_at.ts
@@ -14,16 +14,16 @@ import type { Integer, Negative } from '../number_plus/number.js'
  * type R = ArrayPlus.IndexAt<['a', 'b', 'c'], -1> // 2
  * type R = ArrayPlus.IndexAt<['a', 'b', 'c'], -2> // 1
  * type R = ArrayPlus.IndexAt<['a', 'b', 'c'], -3> // 0
- * type R = ArrayPlus.IndexAt<['a', 'b', 'c'], 3> // undefined
- * type R = ArrayPlus.IndexAt<['a', 'b', 'c'], -4> // undefined
+ * type R = ArrayPlus.IndexAt<['a', 'b', 'c'], 3> // never
+ * type R = ArrayPlus.IndexAt<['a', 'b', 'c'], -4> // never
  * ```
  */
 export type IndexAt<A extends Array<unknown>, N extends number> = Integer<
 	N,
 	Negative<
 		N,
-		GreaterThan<Abs<N>, A['length']> extends true ? undefined : Subtract<A['length'], Abs<N>>,
-		GreaterThan<A['length'], N> extends true ? N : undefined
+		GreaterThan<Abs<N>, A['length']> extends true ? never : Subtract<A['length'], Abs<N>>,
+		GreaterThan<A['length'], N> extends true ? N : never
 	>,
 	never
 >
