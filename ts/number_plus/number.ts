2196f0316487d78d5c6577314afeeeb2028e1a44
unional <homawong@gmail.com>
2023-03-19 15:15:25 -0700

    docs: update type import

--- ts/number_plus/number.ts
+++ ts/number_plus/number.ts
@@ -63,7 +63,7 @@ export type NonNegative<N extends Numeric, Then = N, Else = never> = Negative<N,
  * Check if the type `T` is exactly the type `number` and not numeric literals.
  *
  * ```
- * import { NumberType } from 'type-plus'
+ * import type { NumberType } from 'type-plus'
  *
  * type R = NumberType<number> // true
  * type R = NumberType<1> // false
