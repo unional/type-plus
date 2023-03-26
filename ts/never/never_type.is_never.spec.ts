ef0940422f733ee34ecc5d2b5fbccfa35c77ff88
unional <homawong@gmail.com>
2023-03-25 23:33:23 -0700

    fix: clean up never type doc and tests

--- ts/never/never_type.is_never.spec.ts
+++ ts/never/never_type.is_never.spec.ts
@@ -1,4 +1,5 @@
-import { type, type IsNever, type PrimitiveTypes } from '../index.js'
+// never intersect with any type is never
+import { type, type IsNever } from '../index.js'
 
 it('returns true for never', () => {
 	type.true<IsNever<never>>(true)
@@ -10,10 +11,11 @@ it('returns false for other special types', () => {
 	type.false<IsNever<any>>(true)
 })
 
-test('returns false for singular types', () => {
+test('returns false for other types', () => {
 	type.false<IsNever<undefined>>(true)
 	type.false<IsNever<null>>(true)
 	type.false<IsNever<number>>(true)
+	type.false<IsNever<1>>(true)
 	type.false<IsNever<boolean>>(true)
 	type.false<IsNever<true>>(true)
 	type.false<IsNever<false>>(true)
@@ -21,6 +23,7 @@ test('returns false for singular types', () => {
 	type.false<IsNever<''>>(true)
 	type.false<IsNever<symbol>>(true)
 	type.false<IsNever<bigint>>(true)
+	type.false<IsNever<1n>>(true)
 	type.false<IsNever<{}>>(true)
 	type.false<IsNever<string[]>>(true)
 	type.false<IsNever<[]>>(true)
@@ -29,16 +32,19 @@ test('returns false for singular types', () => {
 })
 
 it('returns false for union type', () => {
-	type.false<IsNever<PrimitiveTypes>>(true)
+	type.false<IsNever<never | 1>>(true)
 })
 
-it('returns false for intersection type', () => {
-	type.false<IsNever<{} & {}>>(true)
+it('returns true for intersection type', () => {
+	// never intersect with any type is never
+	type.true<IsNever<never & { a: 1 }>>(true)
 })
 
 it('can override Then/Else', () => {
+	type.equal<IsNever<never, 1, 2>, 1>(true)
+	type.equal<IsNever<0, 1, 2>, 2>(true)
+
 	type.equal<IsNever<any, 1, 2>, 2>(true)
 	type.equal<IsNever<unknown, 1, 2>, 2>(true)
-	type.equal<IsNever<never, 1, 2>, 1>(true)
 	type.equal<IsNever<void, 1, 2>, 2>(true)
 })
--- ts/never/never_type.is_not_never.spec.ts
+++ ts/never/never_type.is_not_never.spec.ts
@@ -1,4 +1,4 @@
-import { type, type IsNotNever, type PrimitiveTypes } from '../index.js'
+import { type, type IsNotNever } from '../index.js'
 
 it('returns false for never', () => {
 	type.false<IsNotNever<never>>(true)
@@ -14,6 +14,7 @@ test('returns true for singular types', () => {
 	type.true<IsNotNever<undefined>>(true)
 	type.true<IsNotNever<null>>(true)
 	type.true<IsNotNever<number>>(true)
+	type.true<IsNotNever<1>>(true)
 	type.true<IsNotNever<boolean>>(true)
 	type.true<IsNotNever<true>>(true)
 	type.true<IsNotNever<false>>(true)
@@ -21,6 +22,7 @@ test('returns true for singular types', () => {
 	type.true<IsNotNever<''>>(true)
 	type.true<IsNotNever<symbol>>(true)
 	type.true<IsNotNever<bigint>>(true)
+	type.true<IsNotNever<1n>>(true)
 	type.true<IsNotNever<{}>>(true)
 	type.true<IsNotNever<string[]>>(true)
 	type.true<IsNotNever<[]>>(true)
@@ -29,16 +31,19 @@ test('returns true for singular types', () => {
 })
 
 it('returns true for union type', () => {
-	type.true<IsNotNever<PrimitiveTypes>>(true)
+	type.true<IsNotNever<never | 1>>(true)
 })
 
-it('returns true for intersection type', () => {
-	type.true<IsNotNever<{} & {}>>(true)
+it('returns false for intersection type', () => {
+	// never intersect with any type is never
+	type.false<IsNotNever<never & { a: 1 }>>(true)
 })
 
 it('can override Then/Else', () => {
+	type.equal<IsNotNever<never, 1, 2>, 2>(true)
+	type.equal<IsNotNever<0, 1, 2>, 1>(true)
+
 	type.equal<IsNotNever<any, 1, 2>, 1>(true)
 	type.equal<IsNotNever<unknown, 1, 2>, 1>(true)
-	type.equal<IsNotNever<never, 1, 2>, 2>(true)
 	type.equal<IsNotNever<void, 1, 2>, 1>(true)
 })
