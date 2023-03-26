529e12b9a186b084ec68510ed73353140ef05dff
unional <homawong@gmail.com>
2023-03-25 23:32:52 -0700

    test: add intersection tests for undefined

--- ts/undefined/undefined_type.is_not_undefined.spec.ts
+++ ts/undefined/undefined_type.is_not_undefined.spec.ts
@@ -1,4 +1,4 @@
-import { PrimitiveTypes, type, type IsNotUndefined } from '../index.js'
+import { type, type IsNotUndefined, IsUndefined } from '../index.js'
 
 it('returns false for undefined', () => {
 	type.false<IsNotUndefined<undefined>>(true)
@@ -29,11 +29,64 @@ test('returns true for singular types', () => {
 })
 
 it('returns true for union type', () => {
-	type.true<IsNotUndefined<PrimitiveTypes>>(true)
+	type.true<IsNotUndefined<undefined | 1>>(true)
 })
 
-it('returns true for intersection type', () => {
-	type.true<IsNotUndefined<{} & {}>>(true)
+it('returns false as undefined & any => any', () => {
+	type.equal<undefined & any, any>(true)
+	type.false<IsUndefined<undefined & any>>(true)
+})
+
+it('returns false as undefined & unknown => undefined', () => {
+	type.equal<undefined & unknown, undefined>(true)
+	type.false<IsNotUndefined<undefined & unknown>>(true)
+})
+
+it('returns false as undefined & void => undefined', () => {
+	type.equal<undefined & void, undefined>(true)
+	type.false<IsNotUndefined<undefined & void>>(true)
+})
+
+it('returns true as undefined & never => never', () => {
+	type.equal<undefined & never, never>(true)
+	type.true<IsNotUndefined<undefined & never>>(true)
+})
+
+it('returns true as undefined & <others> => never', () => {
+	type.never<undefined & null>(true)
+	type.true<IsNotUndefined<undefined & null>>(true)
+	type.never<undefined & number>(true)
+	type.true<IsNotUndefined<undefined & number>>(true)
+	type.never<undefined & 1>(true)
+	type.true<IsNotUndefined<undefined & 1>>(true)
+	type.never<undefined & boolean>(true)
+	type.true<IsNotUndefined<undefined & boolean>>(true)
+	type.never<undefined & true>(true)
+	type.true<IsNotUndefined<undefined & true>>(true)
+	type.never<undefined & false>(true)
+	type.true<IsNotUndefined<undefined & false>>(true)
+	type.never<undefined & string>(true)
+	type.true<IsNotUndefined<undefined & string>>(true)
+	type.never<undefined & ''>(true)
+	type.true<IsNotUndefined<undefined & ''>>(true)
+	type.never<undefined & symbol>(true)
+	type.true<IsNotUndefined<undefined & symbol>>(true)
+	type.never<undefined & bigint>(true)
+	type.true<IsNotUndefined<undefined & bigint>>(true)
+	type.never<undefined & 1n>(true)
+	type.true<IsNotUndefined<undefined & 1n>>(true)
+	type.never<undefined & {}>(true)
+	type.true<IsNotUndefined<undefined & {}>>(true)
+	type.never<undefined & { a: 1 }>(true)
+	type.true<IsNotUndefined<undefined & { a: 1 }>>(true)
+	type.never<undefined & string[]>(true)
+	type.true<IsNotUndefined<undefined & string[]>>(true)
+	type.never<undefined & []>(true)
+	type.true<IsNotUndefined<undefined & []>>(true)
+	type.never<undefined & Function>(true)
+	type.true<IsNotUndefined<undefined & Function>>(true)
+	type.never<undefined & (() => void)>(true)
+	type.true<IsNotUndefined<undefined & (() => void)>>(true)
 })
 
 it('can override Then/Else', () => {
