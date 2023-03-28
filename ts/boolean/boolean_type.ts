44bac18aacd4585994e23f2b19457207b0dc2e40
unional <homawong@gmail.com>
2023-03-27 22:40:51 -0700

    refactor: move true_type

--- ts/boolean/boolean_type.ts
+++ ts/boolean/boolean_type.ts
@@ -12,7 +12,11 @@ import type { IsAnyOrNever } from '../any/any_or_never.js'
  * type R = FalseType<unknown> // never
  * ```
  */
-export type FalseType<T, Then = T, Else = never> = IsAnyOrNever<T, Else, [T, false] extends [false, T] ? Then : Else>
+export type FalseType<T, Then = T, Else = never> = IsAnyOrNever<
+	T,
+	Else,
+	[T, false] extends [false, T] ? Then : Else
+>
 
 /**
  * Check if the type `T` is not exactly `false`.
@@ -55,62 +59,6 @@ export type IsFalse<T, Then = true, Else = false> = FalseType<T, Then, Else>
  * ```
  */
 export type IsNotFalse<T, Then = true, Else = false> = FalseType<T, Else, Then>
-
-/**
- * Check if the type `T` is exactly `true`.
- *
- * ```ts
- * import type { TrueType } from 'type-plus'
- *
- * type R = TrueType<true> // true
- *
- * type R = TrueType<false> // never
- * type R = TrueType<unknown> // never
- * ```
- */
-export type TrueType<T, Then = T, Else = never> = IsAnyOrNever<T, Else, [T, true] extends [true, T] ? Then : Else>
-
-/**
- * Check if the type `T` is not exactly `true`.
- *
- * ```ts
- * import type { NotTrueType } from 'type-plus'
- *
- * type R = NotTrueType<true> // never
- *
- * type R = NotTrueType<false> // false
- * type R = NotTrueType<unknown> // unknown
- * ```
- */
-export type NotTrueType<T, Then = T, Else = never> = TrueType<T, Else, Then>
-
-/**
- * Is the type `T` exactly `true`.
- *
- * ```ts
- * import type { IsTrue } from 'type-plus'
- *
- * type R = IsTrue<true> // true
- *
- * type R = IsTrue<false> // false
- * type R = IsTrue<unknown> // false
- * ```
- */
-export type IsTrue<T, Then = true, Else = false> = TrueType<T, Then, Else>
-
-/**
- * Is the type `T` not exactly `true`.
- *
- * ```ts
- * import type { IsNotTrue } from 'type-plus'
- *
- * type R = IsNotTrue<true> // false
- *
- * type R = IsNotTrue<false> // true
- * type R = IsNotTrue<unknown> // true
- * ```
- */
-export type IsNotTrue<T, Then = true, Else = false> = TrueType<T, Else, Then>
 
 /**
  * Check if the type `T` is `boolean`, including `true` and `false`.
