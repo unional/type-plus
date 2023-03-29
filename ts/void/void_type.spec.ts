bed07bb46e1d00f6a3206d2e7fe80dbc6055d66b
unional <homawong@gmail.com>
2023-03-28 23:01:42 -0700

    test: add void tests

--- ts/void/void_type.spec.ts
+++ ts/void/void_type.spec.ts
@@ -46,3 +46,60 @@ it('can override Then/Else', () => {
 	type.equal<VoidType<unknown, 1, 2>, 2>(true)
 	type.equal<VoidType<never, 1, 2>, 2>(true)
 })
+
+test('union behavior of void', () => {
+	type.equal<void | undefined, void | undefined>(true)
+	type.equal<void | null, void | null>(true)
+	type.equal<void | boolean, void | boolean>(true)
+	type.equal<void | true, void | true>(true)
+	type.equal<void | false, void | false>(true)
+	type.equal<void | number, void | number>(true)
+	type.equal<void | 1, void | 1>(true)
+	type.equal<void | string, void | string>(true)
+	type.equal<void | '', void | ''>(true)
+	type.equal<void | symbol, void | symbol>(true)
+	type.equal<void | bigint, void | bigint>(true)
+	type.equal<void | 1n, void | 1n>(true)
+	type.equal<void | {}, void | {}>(true)
+	type.equal<void | { a: 1 }, void | { a: 1 }>(true)
+	type.equal<void | string[], void | string[]>(true)
+	type.equal<void | [], void | []>(true)
+	type.equal<void | Function, void | Function>(true)
+	type.equal<void | (() => void), void | (() => void)>(true)
+
+	type.equal<void | any, any>(true)
+	type.equal<void | unknown, unknown>(true)
+	type.equal<void | never, void>(true)
+	type.equal<void | void, void | void>(true)
+})
+
+test('intersection behavior of void', () => {
+	type.equal<void & undefined, undefined>(true)
+	type.equal<void & null, never>(true)
+
+	type.equal<void & boolean, never>(true)
+	type.equal<void & true, never>(true)
+	type.equal<void & false, never>(true)
+	type.equal<void & number, never>(true)
+	type.equal<void & 1, never>(true)
+	type.equal<void & string, never>(true)
+	type.equal<void & '', never>(true)
+
+	type.equal<void & symbol, never>(true)
+
+	type.equal<void & bigint, never>(true)
+	type.equal<void & 1n, never>(true)
+
+	type.equal<void & {}, void & {}>(true)
+
+	type.equal<void & { a: 1 }, void & { a: 1 }>(true)
+	type.equal<void & string[], void & string[]>(true)
+	type.equal<void & [], void & []>(true)
+	type.equal<void & Function, void & Function>(true)
+	type.equal<void & (() => void), void & (() => void)>(true)
+
+	type.equal<void & any, any>(true)
+	type.equal<void & unknown, void>(true)
+	type.equal<void & never, never>(true)
+	type.equal<void & void, void>(true)
+})
