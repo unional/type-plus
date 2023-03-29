099ab5b3f549f4972a8797b21a9993382b58288f
unional <homawong@gmail.com>
2023-03-28 22:10:35 -0700

    feat: add string_type

---
+++ ts/string/string_type.spec.ts
@@ -0,0 +1,49 @@
+import { type, type StringType } from '../index.js'
+
+it('returns T if T is string', () => {
+	type.equal<StringType<string>, string>(true)
+})
+
+it('returns T if T is a string literal', () => {
+	type.equal<StringType<''>, ''>(true)
+	type.equal<StringType<'a'>, 'a'>(true)
+})
+
+it('returns never for special types', () => {
+	type.never<StringType<any>>(true)
+	type.never<StringType<unknown>>(true)
+	type.never<StringType<void>>(true)
+	type.never<StringType<never>>(true)
+})
+
+test('returns never for other types', () => {
+	type.never<StringType<undefined>>(true)
+	type.never<StringType<null>>(true)
+	type.never<StringType<boolean>>(true)
+	type.never<StringType<true>>(true)
+	type.never<StringType<false>>(true)
+	type.never<StringType<number>>(true)
+	type.never<StringType<1>>(true)
+	type.never<StringType<symbol>>(true)
+	type.never<StringType<bigint>>(true)
+	type.never<StringType<{}>>(true)
+	type.never<StringType<string[]>>(true)
+	type.never<StringType<[]>>(true)
+	type.never<StringType<Function>>(true)
+	type.never<StringType<() => void>>(true)
+})
+
+it('returns never for union type', () => {
+	type.never<StringType<string | 1>>(true)
+})
+
+it('can override Then/Else', () => {
+	type.equal<StringType<string, 1, 2>, 1>(true)
+	type.equal<StringType<'', 1, 2>, 1>(true)
+	type.equal<StringType<'a', 1, 2>, 1>(true)
+
+	type.equal<StringType<any, 1, 2>, 2>(true)
+	type.equal<StringType<unknown, 1, 2>, 2>(true)
+	type.equal<StringType<never, 1, 2>, 2>(true)
+	type.equal<StringType<void, 1, 2>, 2>(true)
+})
---
+++ ts/string/string_type.ts
@@ -0,0 +1,69 @@
+import type { IsAnyOrNever } from '../any/any_or_never.js'
+
+/**
+ * Check if the type `T` is `string` or string literals.
+ *
+ * ```
+ * import type { StringType } from 'type-plus'
+ *
+ * type R = StringType<string> // string
+ * type R = StringType<''> // ''
+ * type R = StringType<'1'> // '1'
+ *
+ * type R = StringType<string | boolean> // never
+ * type R = StringType<never> // never
+ * type R = StringType<unknown> // never
+ * ```
+ */
+export type StringType<T, Then = T, Else = never> = IsAnyOrNever<T, Else, [T] extends [string] ? Then : Else>
+
+/**
+ * Is the type `T` `string` or string literals.
+ *
+ * ```
+ * import type { IsString } from 'type-plus'
+ *
+ * type R = IsString<string> // true
+ * type R = IsString<''> // true
+ * type R = IsString<'a'> // true
+ *
+ * type R = IsString<string | boolean> // false
+ * type R = IsString<never> // false
+ * type R = IsString<unknown> // false
+ * ```
+ */
+export type IsString<T, Then = true, Else = false> = StringType<T, Then, Else>
+
+/**
+ * Check if the type `T` is not `string` nor string literals.
+ *
+ * ```
+ * import type { NotStringType } from 'type-plus'
+ *
+ * type R = NotStringType<string> // never
+ * type R = NotStringType<''> // ''
+ * type R = NotStringType<'a'> // 'a'
+ *
+ * type R = NotStringType<string | boolean> // string | boolean
+ * type R = NotStringType<never> // never
+ * type R = NotStringType<unknown> // unknown
+ * ```
+ */
+export type NotStringType<T, Then = T, Else = never> = StringType<T, Else, Then>
+
+/**
+ * Is the type `T` not `string` nor string literals.
+ *
+ * ```
+ * import type { IsNotString } from 'type-plus'
+ *
+ * type R = IsNotString<string> // false
+ * type R = IsNotString<''> // false
+ * type R = IsNotString<'a'> // false
+ *
+ * type R = IsNotString<string | boolean> // true
+ * type R = IsNotString<never> // true
+ * type R = IsNotString<unknown> // true
+ * ```
+ */
+export type IsNotString<T, Then = true, Else = false> = StringType<T, Else, Then>
