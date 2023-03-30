15abecf85c9c8e352e64c32a762b8889d17cc7e8
unional <homawong@gmail.com>
2023-03-29 23:36:00 -0700

    docs: add readme for function

--- ts/function/extract_function.ts
+++ ts/function/extract_function.ts
@@ -5,6 +5,15 @@ import type { AnyFunction } from './any_function.js'
  *
  * It works with interact of functions, but not on function overloads and union.
  * @note does not work with function overloads.
+ *
+ * ```ts
+ * import type { ExtractFunction } from 'type-plus'
+ *
+ * type R = ExtractFunction<{
+ *   () => void
+ *   a: 1
+ * }> // () => void
+ * ```
  */
 export type ExtractFunction<T extends AnyFunction> = T extends AnyFunction<infer P, infer R>
 	? (...args: P) => R
@@ -16,5 +25,5 @@ export type ExtractFunction<T extends AnyFunction> = T extends AnyFunction<infer
  * @note does not work with function overloads.
  */
 export function extractFunction<T extends AnyFunction>(fn: T) {
-	return fn as any as ExtractFunction<T>
+	return fn as unknown as ExtractFunction<T>
 }
