4e0acadc77b1e1c1eef0d6f1a6ec71b5410d1f16
unional <homawong@gmail.com>
2023-05-21 01:18:42 -0700

    chore: add DigitAdd/Subtract/Multiply

--- ts/math/math_device.ts
+++ ts/math/math_device.ts
@@ -79,4 +79,50 @@ export namespace MathDevice {
 	type NumberArrayToString<N extends number[]> = N['length'] extends 0
 		? ''
 		: `${N[0]}${NumberArrayToString<Tail<N>>}`
+
+	export type Normalize<M extends MathDevice> = M[0] extends 'bigint'
+		? NormalizeBigint<M>
+		: NormalizeNumber<M>
+
+	type NormalizeBigint<M extends MathDevice> = M
+	type NormalizeNumber<M extends MathDevice> = M
+
+	export type DigitAdd<A extends number, B extends number> = [
+		[0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+		[1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+		[2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+		[3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
+		[4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
+		[5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
+		[6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+		[7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
+		[8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
+		[9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
+	][A][B]
+
+	export type DigitSubtract<A extends number, B extends number> = [
+		[0, -1, -2, -3, -4, -5, -6, -7, -8, -9],
+		[1, 0, -1, -2, -3, -4, -5, -6, -7, -8],
+		[2, 1, 0, -1, -2, -3, -4, -5, -6, -7],
+		[3, 2, 1, 0, -1, -2, -3, -4, -5, -6],
+		[4, 3, 2, 1, 0, -1, -2, -3, -4, -5],
+		[5, 4, 3, 2, 1, 0, -1, -2, -3, -4],
+		[6, 5, 4, 3, 2, 1, 0, -1, -2, -3],
+		[7, 6, 5, 4, 3, 2, 1, 0, -1, -2],
+		[8, 7, 6, 5, 4, 3, 2, 1, 0, -1],
+		[9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
+	][A][B]
+
+	export type DigitMultiply<A extends number, B extends number> = [
+		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+		[0, 2, 4, 6, 8, 10, 12, 14, 16, 18],
+		[0, 3, 6, 9, 12, 15, 18, 21, 24, 27],
+		[0, 4, 8, 12, 16, 20, 24, 28, 32, 36],
+		[0, 5, 10, 15, 20, 25, 30, 35, 40, 45],
+		[0, 6, 12, 18, 24, 30, 36, 42, 48, 54],
+		[0, 7, 14, 21, 28, 35, 42, 49, 56, 63],
+		[0, 8, 16, 24, 32, 40, 48, 56, 64, 72],
+		[0, 9, 18, 27, 36, 45, 54, 63, 72, 81]
+	][A][B]
 }
