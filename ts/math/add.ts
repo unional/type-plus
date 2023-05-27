7432e76d84568bf3d4eb9941fd26c8bdc89bdc3e
unional <homawong@gmail.com>
2023-05-27 16:52:24 -0700

    refactor: rename and organize MathStruct to NumericStruct

--- ts/math/add.ts
+++ ts/math/add.ts
@@ -1,50 +1,45 @@
-import {
-	MathStruct,
-	NormalizeMathStruct,
-	NormalizedMathStructToNumeric,
-	NumericToMathStruct
-} from './numeric_struct.js'
+import { NumericStruct } from './numeric_struct.js'
 
 import { DigitsStruct } from './digit_struct.js'
 
 export type Add<A extends number | bigint, B extends number | bigint, Fail = never> = [
-	NumericToMathStruct<A>,
-	NumericToMathStruct<B>
-] extends [infer MA extends MathStruct, infer MB extends MathStruct]
+	NumericStruct.FromNumeric<A>,
+	NumericStruct.FromNumeric<B>
+] extends [infer MA extends NumericStruct, infer MB extends NumericStruct]
 	? [MA[1], MB[1]] extends ['+', '+']
-		? NormalizedMathStructToNumeric<
-				NormalizeMathStruct<[MA[0], '+', DigitsStruct.AddNormalized<MA[2], MB[2]>]>
+		? NumericStruct.NormalizedToNumeric<
+				NumericStruct.Normalize<[MA[0], '+', DigitsStruct.AddNormalized<MA[2], MB[2]>]>
 		  >
 		: [MA[1], MB[1]] extends ['+', '-']
-		? NormalizedMathStructToNumeric<
-				NormalizeMathStruct<[MA[0], '+', DigitsStruct.SubtractNormalized<MA[2], MB[2], []>]>
+		? NumericStruct.NormalizedToNumeric<
+				NumericStruct.Normalize<[MA[0], '+', DigitsStruct.SubtractNormalized<MA[2], MB[2], []>]>
 		  >
 		: [MA[1], MB[1]] extends ['-', '+']
-		? NormalizedMathStructToNumeric<
-				NormalizeMathStruct<[MA[0], '+', DigitsStruct.SubtractNormalized<MB[2], MA[2], []>]>
+		? NumericStruct.NormalizedToNumeric<
+				NumericStruct.Normalize<[MA[0], '+', DigitsStruct.SubtractNormalized<MB[2], MA[2], []>]>
 		  >
-		: NormalizedMathStructToNumeric<
-				NormalizeMathStruct<[MA[0], '-', DigitsStruct.AddNormalized<MA[2], MB[2]>]>
+		: NumericStruct.NormalizedToNumeric<
+				NumericStruct.Normalize<[MA[0], '-', DigitsStruct.AddNormalized<MA[2], MB[2]>]>
 		  >
 	: never
 
 export type Subtract<A extends number | bigint, B extends number | bigint, Fail = never> = [
-	NumericToMathStruct<A>,
-	NumericToMathStruct<B>
-] extends [infer MA extends MathStruct, infer MB extends MathStruct]
+	NumericStruct.FromNumeric<A>,
+	NumericStruct.FromNumeric<B>
+] extends [infer MA extends NumericStruct, infer MB extends NumericStruct]
 	? [MA[1], MB[1]] extends ['+', '+']
-		? NormalizedMathStructToNumeric<
-				NormalizeMathStruct<[MA[0], '+', DigitsStruct.SubtractNormalized<MA[2], MB[2], []>]>
+		? NumericStruct.NormalizedToNumeric<
+				NumericStruct.Normalize<[MA[0], '+', DigitsStruct.SubtractNormalized<MA[2], MB[2], []>]>
 		  >
 		: [MA[1], MB[1]] extends ['+', '-']
-		? NormalizedMathStructToNumeric<
-				NormalizeMathStruct<[MA[0], '+', DigitsStruct.AddNormalized<MA[2], MB[2]>]>
+		? NumericStruct.NormalizedToNumeric<
+				NumericStruct.Normalize<[MA[0], '+', DigitsStruct.AddNormalized<MA[2], MB[2]>]>
 		  >
 		: [MA[1], MB[1]] extends ['-', '+']
-		? NormalizedMathStructToNumeric<
-				NormalizeMathStruct<[MA[0], '-', DigitsStruct.AddNormalized<MA[2], MB[2]>]>
+		? NumericStruct.NormalizedToNumeric<
+				NumericStruct.Normalize<[MA[0], '-', DigitsStruct.AddNormalized<MA[2], MB[2]>]>
 		  >
-		: NormalizedMathStructToNumeric<
-				NormalizeMathStruct<[MA[0], '+', DigitsStruct.SubtractNormalized<MB[2], MA[2], []>]>
+		: NumericStruct.NormalizedToNumeric<
+				NumericStruct.Normalize<[MA[0], '+', DigitsStruct.SubtractNormalized<MB[2], MA[2], []>]>
 		  >
 	: never
