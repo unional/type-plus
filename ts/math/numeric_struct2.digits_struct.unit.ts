379ec7124f7d73e287c217339bc2e756d91b4a5d
unional <homawong@gmail.com>
2023-05-28 22:59:20 -0700

    chore: add flip sign support

--- ts/math/numeric_struct2.digits_struct.unit.ts
+++ ts/math/numeric_struct2.digits_struct.unit.ts
@@ -8,25 +8,40 @@ describe('Normalize', () => {
 		testType.equal<DigitsStruct.Normalize<['+', [9], 0]>, ['+', [9], 0]>(true)
 		testType.equal<DigitsStruct.Normalize<['+', [1, 2, 3], 0]>, ['+', [1, 2, 3], 0]>(true)
 		testType.equal<DigitsStruct.Normalize<['+', [1, 2, 3], 1]>, ['+', [1, 2, 3], 1]>(true)
-		testType.equal<DigitsStruct.Normalize<['+', [0, 0, 1, 2, 3], 4]>, ['+', [0, 0, 1, 2, 3], 4]>(true)
+		testType.equal<DigitsStruct.Normalize<['+', [0, 0, 1, 2, 3], 4]>, ['+', [1, 2, 3], 4]>(true)
 
 		testType.equal<DigitsStruct.Normalize<['-', [0], 0]>, ['-', [0], 0]>(true)
 		testType.equal<DigitsStruct.Normalize<['-', [9], 0]>, ['-', [9], 0]>(true)
 		testType.equal<DigitsStruct.Normalize<['-', [1, 2, 3], 0]>, ['-', [1, 2, 3], 0]>(true)
 		testType.equal<DigitsStruct.Normalize<['-', [1, 2, 3], 1]>, ['-', [1, 2, 3], 1]>(true)
-		testType.equal<DigitsStruct.Normalize<['-', [0, 0, 1, 2, 3], 4]>, ['-', [0, 0, 1, 2, 3], 4]>(true)
+		testType.equal<DigitsStruct.Normalize<['-', [0, 0, 1, 2, 3], 4]>, ['-', [1, 2, 3], 4]>(true)
 	})
 
-	// it('advance digits when digit > 10', () => {
-	// 	testType.equal<DigitsStruct.Normalize<['+', [10], 0]>, ['+', [1, 0], 0]>(true)
+	it('carry over when digit > 10', () => {
+		testType.equal<DigitsStruct.Normalize<['+', [10], 0]>, ['+', [1, 0], 0]>(true)
 
-	// 	testType.equal<DigitsStruct.Normalize<['+', [81], 0]>, ['+', [8, 1], 0]>(true)
+		testType.equal<DigitsStruct.Normalize<['+', [81], 0]>, ['+', [8, 1], 0]>(true)
 
-	// 	testType.equal<DigitsStruct.Normalize<['+', [1, 10], 0]>, ['+', [2, 0], 0]>(true)
-	// 	testType.equal<DigitsStruct.Normalize<['+', [1, 81], 0]>, ['+', [9, 1], 0]>(true)
+		testType.equal<DigitsStruct.Normalize<['+', [1, 10], 0]>, ['+', [2, 0], 0]>(true)
+		testType.equal<DigitsStruct.Normalize<['+', [1, 81], 0]>, ['+', [9, 1], 0]>(true)
 
-	// 	testType.equal<DigitsStruct.Normalize<['+', [81, 81], 0]>, ['+', [8, 9, 1], 0]>(true)
+		testType.equal<DigitsStruct.Normalize<['+', [81, 81], 0]>, ['+', [8, 9, 1], 0]>(true)
 
-	// 	testType.equal<DigitsStruct.Normalize<['+', [9, 9, 9, 9, 10], 0]>, ['+', [1, 0, 0, 0, 0, 0], 0]>(true)
-	// })
+		testType.equal<DigitsStruct.Normalize<['+', [9, 9, 9, 9, 10], 0]>, ['+', [1, 0, 0, 0, 0, 0], 0]>(true)
+	})
+
+	it('carry over when digit < 0 (the most negative it can get is -10', () => {
+		testType.equal<DigitsStruct.Normalize<['+', [2, -1], 0]>, ['+', [1, 9], 0]>(true)
+		testType.equal<DigitsStruct.Normalize<['+', [2, -10, 3], 0]>, ['+', [1, 0, 3], 0]>(true)
+		testType.equal<DigitsStruct.Normalize<['+', [1, -1], 0]>, ['+', [9], 0]>(true)
+
+		testType.equal<DigitsStruct.Normalize<['-', [2, -1], 0]>, ['-', [1, 9], 0]>(true)
+		testType.equal<DigitsStruct.Normalize<['-', [2, -10, 3], 0]>, ['-', [1, 0, 3], 0]>(true)
+		testType.equal<DigitsStruct.Normalize<['-', [1, -1], 0]>, ['-', [9], 0]>(true)
+	})
+
+	it('flips sign when first digit is negative, and then normalize', () => {
+		testType.equal<DigitsStruct.Normalize<['+', [-1, 2, -3], 0]>, ['-', [8, 3], 0]>(true)
+		testType.equal<DigitsStruct.Normalize<['-', [-1, 2, -3], 0]>, ['+', [8, 3], 0]>(true)
+	})
 })
