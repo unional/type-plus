dc32db3805c05cadbe004d2a90b79331e483af70
unional <homawong@gmail.com>
2023-05-29 19:18:39 -0700

    feat: GreaterThan and Max support bigint, negative, and floating point

--- ts/math/Max.spec.ts
+++ ts/math/Max.spec.ts
@@ -1,39 +1,39 @@
-import { test } from '@jest/globals'
-import { Max, testType } from '../index.js'
+import { it } from '@jest/globals'
+import { testType, type Max } from '../index.js'
 
-test('only support whole number', () => {
-	testType.never<Max<0.1, 1>>(true)
-	testType.never<Max<1, 0.1>>(true)
+it('works with floating point', () => {
+	testType.equal<Max<0.1, 1>, 1>(true)
+	testType.equal<Max<1, 0.1>, 1>(true)
 })
 
-test('number gets never', () => {
+it('number gets never', () => {
 	testType.never<Max<number, 1>>(true)
 	testType.never<Max<1, number>>(true)
 })
 
-test('override Fail case', () => {
+it('override Fail case', () => {
 	testType.strictNumber<Max<number, 1, number>>(true)
 })
 
-test('same number', () => {
+it('same number', () => {
 	testType.equal<Max<123, 123>, 123>(true)
 })
 
-test('same number of digits', () => {
+it('same number of digits', () => {
 	testType.equal<Max<123, 124>, 124>(true)
 	testType.equal<Max<126, 124>, 126>(true)
 	testType.equal<Max<126, 224>, 224>(true)
 	testType.equal<Max<726, 224>, 726>(true)
 })
 
-test('more digits win', () => {
+it('more digits win', () => {
 	testType.equal<Max<123, 22>, 123>(true)
 	testType.equal<Max<12, 223>, 223>(true)
 	testType.equal<Max<1234567891011, 123>, 1234567891011>(true)
 	testType.equal<Max<123, 1234567891011>, 1234567891011>(true)
 })
 
-test('work with negative numbers', () => {
+it('work with negative numbers', () => {
 	testType.equal<Max<0, -0>, 0>(true)
 	testType.equal<Max<-0, 0>, 0>(true)
 	testType.equal<Max<-0, -0>, 0>(true)
@@ -51,7 +51,7 @@ test('work with negative numbers', () => {
 	testType.equal<Max<-3, -2>, -2>(true)
 })
 
-test('work with large numbers', () => {
+it('work with large numbers', () => {
 	testType.equal<Max<1000000, 0>, 1000000>(true)
 	testType.equal<Max<0, 1000000>, 1000000>(true)
 
--- ts/math/Max.ts
+++ ts/math/Max.ts
@@ -1,8 +1,11 @@
-import type { IsEqual } from '../equal/equal.js'
+import type { IsNever } from '../never/never_type.js'
 import type { GreaterThan } from './greater_than.js'
 
-export type Max<A extends number, B extends number, Fail = never> = GreaterThan<A, B> extends infer Result
-	? IsEqual<Result, never> extends true
+export type Max<A extends number | bigint, B extends number | bigint, Fail = never> = GreaterThan<
+	A,
+	B
+> extends infer Result
+	? IsNever<Result> extends true
 		? Fail
 		: Result extends true
 		? A
