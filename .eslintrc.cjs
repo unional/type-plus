4a00b634feec3d57642e66b7bd2bafd6371f6817
unional <homawong@gmail.com>
2023-05-31 21:53:23 -0700

    chore: update lint config

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+	env: {
+		node: true,
+		es6: true
+	},
+	ignorePatterns: ['docs', 'node_modules'],
+	parserOptions: {
+		sourceType: 'module'
+	},
+	extends: ['plugin:harmony/latest', 'plugin:yml/standard'],
+	rules: {
+		'yml/quotes': ['error', { prefer: 'single' }]
+	},
+	root: true
+}
