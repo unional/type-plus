7c8e0e83dde2c2abe49e26de50d3c60fcb610ff4
unional <homawong@gmail.com>
2023-07-09 15:20:19 -0700

    chore: update test

---
+++ type-plus/ts/array/array_plus.is_index_out_of_bound.spec.ts
@@ -0,0 +1,31 @@
+import { it } from '@jest/globals'
+import { testType, type ArrayPlus } from '../index.js'
+
+it('returns true for never', () => {
+	testType.true<ArrayPlus.IsIndexOutOfBound<['a'], never>>(true)
+})
+
+it('returns true for empty tuple', () => {
+	testType.true<ArrayPlus.IsIndexOutOfBound<[], never>>(true)
+	testType.true<ArrayPlus.IsIndexOutOfBound<[], 0>>(true)
+})
+
+it('return false for array', () => {
+	testType.false<ArrayPlus.IsIndexOutOfBound<any[], 0>>(true)
+	testType.false<ArrayPlus.IsIndexOutOfBound<any[], 1>>(true)
+	testType.false<ArrayPlus.IsIndexOutOfBound<any[], -1>>(true)
+})
+
+it('returns true for out of bound index', () => {
+	testType.true<ArrayPlus.IsIndexOutOfBound<['a'], 1>>(true)
+	testType.true<ArrayPlus.IsIndexOutOfBound<['a'], -2>>(true)
+})
+
+it('returns false for in bound index', () => {
+	testType.false<ArrayPlus.IsIndexOutOfBound<['a'], 0>>(true)
+	testType.false<ArrayPlus.IsIndexOutOfBound<['a'], -1>>(true)
+})
+
+it('supports readonly array', () => {
+	testType.true<ArrayPlus.IsIndexOutOfBound<readonly ['a'], 1>>(true)
+})
