0771911ea67bd4904dee3bc9dfa0cede3ace4953
unional <homawong@gmail.com>
2023-09-12 21:50:19 -0700

    test: add AnyRecord behavior showcase

--- type-plus/ts/object/any_record.spec.ts
+++ type-plus/ts/object/any_record.spec.ts
@@ -12,3 +12,13 @@ it('use as type criteria', () => {
 	// @ts-expect-error
 	testType.equal<X<never>, never>(true)
 })
+
+it('shows how AnyRecord behaves as criteria against primitive types', () => {
+	testType.equal<Boolean extends AnyRecord ? true : false, true>(true)
+	testType.equal<Number extends AnyRecord ? true : false, true>(true)
+	testType.equal<String extends AnyRecord ? true : false, true>(true)
+	testType.equal<Function extends AnyRecord ? true : false, true>(true)
+	testType.equal<Object extends AnyRecord ? true : false, true>(true)
+	testType.equal<Symbol extends AnyRecord ? true : false, true>(true)
+	testType.equal<BigInt extends AnyRecord ? true : false, true>(true)
+})
