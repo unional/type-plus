982a3b7ee195633b1a81a3dedd54515357d15dbe
unional <homawong@gmail.com>
2023-09-17 20:43:04 -0700

    docs: update docs for $Error

--- type-plus/ts/type_plus/type.ts
+++ type-plus/ts/type_plus/type.ts
@@ -1,4 +1,6 @@
 /**
+ * 🧰 *type util*
+ *
  * Special `type-plus` interface to represent unique types with messages.
  */
 export interface $Type<T extends string, M extends string = '', V = unknown> {
