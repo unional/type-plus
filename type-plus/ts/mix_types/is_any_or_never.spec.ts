f4ab43f827f9a3337c5d4fa5f435bac92e3538f3
unional <homawong@gmail.com>
2023-09-18 23:52:58 -0700

    feat: default IsAnyOrNever to use branch
    remove AnyOrNeverType

--- type-plus/ts/mix_types/is_any_or_never.spec.ts
+++ type-plus/ts/mix_types/is_any_or_never.spec.ts
@@ -1,63 +1,63 @@
 import { it } from '@jest/globals'
-import { testType, type IsAnyOrNever } from '../index.js'
+import { testType, type IsAnyOrNever, type $Then, type $Else, type $SelectionPredicate } from '../index.js'
 
 it('returns true for any', () => {
-	testType.equal<IsAnyOrNever<any>, true>(true)
+	testType.equal<IsAnyOrNever<any>, $Then>(true)
 })
 
 it('returns true for never', () => {
-	testType.equal<IsAnyOrNever<never>, true>(true)
+	testType.equal<IsAnyOrNever<never>, $Then>(true)
 })
 
 it('returns false for other special types', () => {
-	testType.false<IsAnyOrNever<unknown>>(true)
-	testType.false<IsAnyOrNever<void>>(true)
+	testType.equal<IsAnyOrNever<unknown>, $Else>(true)
+	testType.equal<IsAnyOrNever<void>, $Else>(true)
 })
 
 it('returns never for other types', () => {
-	testType.false<IsAnyOrNever<undefined>>(true)
-	testType.false<IsAnyOrNever<null>>(true)
-	testType.false<IsAnyOrNever<boolean>>(true)
-	testType.false<IsAnyOrNever<true>>(true)
-	testType.false<IsAnyOrNever<false>>(true)
-	testType.false<IsAnyOrNever<number>>(true)
-	testType.false<IsAnyOrNever<1>>(true)
-	testType.false<IsAnyOrNever<string>>(true)
-	testType.false<IsAnyOrNever<''>>(true)
-	testType.false<IsAnyOrNever<symbol>>(true)
-	testType.false<IsAnyOrNever<bigint>>(true)
-	testType.false<IsAnyOrNever<1n>>(true)
-	testType.false<IsAnyOrNever<{}>>(true)
-	testType.false<IsAnyOrNever<{ a: 1 }>>(true)
-	testType.false<IsAnyOrNever<string[]>>(true)
-	testType.false<IsAnyOrNever<[]>>(true)
-	testType.false<IsAnyOrNever<Function>>(true)
-	testType.false<IsAnyOrNever<() => void>>(true)
+	testType.equal<IsAnyOrNever<undefined>, $Else>(true)
+	testType.equal<IsAnyOrNever<null>, $Else>(true)
+	testType.equal<IsAnyOrNever<boolean>, $Else>(true)
+	testType.equal<IsAnyOrNever<true>, $Else>(true)
+	testType.equal<IsAnyOrNever<false>, $Else>(true)
+	testType.equal<IsAnyOrNever<number>, $Else>(true)
+	testType.equal<IsAnyOrNever<1>, $Else>(true)
+	testType.equal<IsAnyOrNever<string>, $Else>(true)
+	testType.equal<IsAnyOrNever<''>, $Else>(true)
+	testType.equal<IsAnyOrNever<symbol>, $Else>(true)
+	testType.equal<IsAnyOrNever<bigint>, $Else>(true)
+	testType.equal<IsAnyOrNever<1n>, $Else>(true)
+	testType.equal<IsAnyOrNever<{}>, $Else>(true)
+	testType.equal<IsAnyOrNever<{ a: 1 }>, $Else>(true)
+	testType.equal<IsAnyOrNever<string[]>, $Else>(true)
+	testType.equal<IsAnyOrNever<[]>, $Else>(true)
+	testType.equal<IsAnyOrNever<Function>, $Else>(true)
+	testType.equal<IsAnyOrNever<() => void>, $Else>(true)
 })
 
 it('returns true for `any | 1` because that is resolved to `any` by TypeScript', () => {
 	testType.equal<any | 1, any>(true)
-	testType.true<IsAnyOrNever<any | 1>>(true)
+	testType.equal<IsAnyOrNever<any | 1>, $Then>(true)
 })
 
 it('returns false for `never | 1` because that is resolved to `1` by TypeScript', () => {
 	testType.equal<never | 1, 1>(true)
-	testType.false<IsAnyOrNever<never | 1>>(true)
+	testType.equal<IsAnyOrNever<never | 1>, $Else>(true)
 })
 
 it('returns true for intersection type', () => {
 	testType.equal<any & 1, any>(true)
-	testType.true<IsAnyOrNever<any & 1>>(true)
+	testType.equal<IsAnyOrNever<any & 1>, $Then>(true)
 
 	testType.equal<never & 1, never>(true)
-	testType.true<IsAnyOrNever<never & 1>>(true)
+	testType.equal<IsAnyOrNever<never & 1>, $Then>(true)
 })
 
 it('can override Then/Else', () => {
-	testType.equal<IsAnyOrNever<any, { $then: 1, $else: 2 }>, 1>(true)
-	testType.equal<IsAnyOrNever<never, { $then: 1, $else: 2 }>, 1>(true)
+	testType.equal<IsAnyOrNever<any, $SelectionPredicate>, true>(true)
+	testType.equal<IsAnyOrNever<never, $SelectionPredicate>, true>(true)
 
-	testType.equal<IsAnyOrNever<0, { $then: 1, $else: 2 }>, 2>(true)
-	testType.equal<IsAnyOrNever<unknown, { $then: 1, $else: 2 }>, 2>(true)
-	testType.equal<IsAnyOrNever<void, { $then: 1, $else: 2 }>, 2>(true)
+	testType.equal<IsAnyOrNever<0, $SelectionPredicate>, false>(true)
+	testType.equal<IsAnyOrNever<unknown, $SelectionPredicate>, false>(true)
+	testType.equal<IsAnyOrNever<void, $SelectionPredicate>, false>(true)
 })
--- type-plus/ts/mix_types/is_any_or_never.ts
+++ type-plus/ts/mix_types/is_any_or_never.ts
@@ -1,6 +1,6 @@
 import type { AnyType } from '../any/any_type.js'
 import type { NeverType } from '../never/never_type.js'
-import type { $SelectionOptions, $SelectionPredicate } from '../type_plus/branch/selection.js'
+import type { $SelectionBranch, $SelectionOptions } from '../type_plus/branch/selection.js'
 
 /**
  * 🎭 *predicate*
@@ -11,16 +11,19 @@ import type { $SelectionOptions, $SelectionPredicate } from '../type_plus/branch
  *
  * @example
  * ```ts
- * type R = IsAnyOrNever<any> // true
- * type R = IsAnyOrNever<never> // true
+ * type R = IsAnyOrNever<any> // $Then
+ * type R = IsAnyOrNever<never> // $Then
  *
- * type R = IsAnyOrNever<1> // false
- * type R = IsAnyOrNever<unknown> // false
+ * type R = IsAnyOrNever<1> // $Else
+ * type R = IsAnyOrNever<unknown> // $Else
+ *
+ * type R = IsAnyOrNever<never, $SelectionPredicate> // true
+ * type R = IsAnyOrNever<'a', $SelectionPredicate> // false
  * ```
  */
 export type IsAnyOrNever<
 	T,
-	$Options extends $SelectionOptions = $SelectionPredicate
+	$Options extends $SelectionOptions = $SelectionBranch
 > = NeverType<T, {
 	$then: $Options['$then'],
 	$else: AnyType<T, $Options>
