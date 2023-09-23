29a9a14ead60349fc12077987883b57a4ba5dabb
unional <homawong@gmail.com>
2023-09-23 15:54:37 -0700

    fix: simplify resolve options

--- type-plus/ts/type_plus/resolve_options.ts
+++ type-plus/ts/type_plus/resolve_options.ts
@@ -17,9 +17,7 @@ export type $ResolveOptions<Values extends unknown[]> =
 
 export namespace $ResolveOptions {
 	export type _<V extends unknown[], R> = V extends [infer T]
-		? (0 extends 1 & T
-			? T
-			: ([T, unknown] extends [unknown, T] ? unknown : T))
+		? T
 		: (V extends [infer T, ...infer U]
 			? ([T, unknown] extends [unknown, T] ? _<U, R> : T)
 			: $InferError<'recursive value', V>)
