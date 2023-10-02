809305090a9ae8a55b611f1adfd253134fdb29a1
unional <homawong@gmail.com>
2023-10-02 00:39:42 -0700

    fix: NumberPlus ref

--- type-plus/ts/number/number_plus.ts
+++ type-plus/ts/number/number_plus.ts
@@ -1,16 +1,23 @@
-export type { Integer, IsInteger, IsNotInteger, IsWhole, NotInteger } from '../numeric/integer.js'
-export type { IsNegative, IsNotNegative, Negative, NotNegative } from '../numeric/negative.js'
+export type { Integer, IsWhole, NotInteger } from '../numeric/integer.js'
+export type * from '../numeric/is_integer.js'
+export type * from '../numeric/is_negative.js'
+export type * from '../numeric/is_not_integer.js'
+export type * from '../numeric/is_not_negative.js'
+export type * from '../numeric/is_not_numeric.js'
+export type * from '../numeric/is_not_positive.js'
+export type * from '../numeric/is_numeric.js'
+export type * from '../numeric/is_positive.js'
+export type { Negative, NotNegative } from '../numeric/negative.js'
 export type {
-	IsNotNumeric,
-	IsNumeric,
 	NotNumericType,
 	Numeric,
 	NumericType,
 	Zero
 } from '../numeric/numeric_type.js'
-export type { IsNotPositive, IsPositive, NonPositive, Positive } from '../numeric/positive.js'
+export type { NonPositive, Positive } from '../numeric/positive.js'
 export type * from './is_not_number.js'
 export type * from './is_not_strict_number.js'
 export type * from './is_number.js'
 export type * from './is_strict_number.js'
+
 // export type { Sum } from './number_array.js'
