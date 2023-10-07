3d5aa57f5a444404857e7ba6d711fecdb7940a1a
unional <homawong@gmail.com>
2023-10-07 13:17:03 -0700

    fix: remore $IsBranch
    It's not used anymore

--- type-plus/ts/type_plus/branch/is_branch.spec.ts
+++
@@ -1,6 +0,0 @@
-import { it } from '@jest/globals'
-import { testType, type $Any, type $IsBranch } from '../../index.js'
-
-it('detects if type is branch', () => {
-	testType.true<$IsBranch<$Any>>(true)
-})
