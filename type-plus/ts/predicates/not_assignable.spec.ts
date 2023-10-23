68500b59a60c65b43cadc02e11cec9b862bf0fb7
unional <homawong@gmail.com>
2023-10-23 07:08:45 +0000

    feat: add NotAssignable

---
+++ type-plus/ts/predicates/not_assignable.spec.ts
@@ -0,0 +1,115 @@
+import { it } from '@jest/globals'
+
+import { type $Else, type $Then, type NotAssignable, testType } from '../index.js'
+
+it('check if A can be assigned to B', () => {
+	testType.false<NotAssignable<1, 1>>(true)
+	testType.false<NotAssignable<1, number>>(true)
+	testType.false<NotAssignable<number, number>>(true)
+	testType.false<NotAssignable<'a', 'a'>>(true)
+	testType.false<NotAssignable<'a', string>>(true)
+	testType.false<NotAssignable<string, string>>(true)
+	testType.false<NotAssignable<false, boolean>>(true)
+	testType.false<NotAssignable<true, boolean>>(true)
+	testType.false<NotAssignable<boolean, boolean>>(true)
+	testType.false<NotAssignable<{ a: 1 }, { a: number }>>(true)
+	testType.false<NotAssignable<{ a: string, b: number }, { a: string }>>(true)
+
+	testType.true<NotAssignable<number, 1>>(true)
+	testType.true<NotAssignable<string, 'a'>>(true)
+	testType.true<NotAssignable<{ a: number }, { a: 1 }>>(true)
+	testType.true<NotAssignable<{ a: string }, { a: string, b: number }>>(true)
+})
+
+it('returns true when B is `any` as anything can be assigned to `any`', () => {
+	testType.false<NotAssignable<any, any>>(true)
+	testType.false<NotAssignable<unknown, any>>(true)
+	testType.false<NotAssignable<never, any>>(true)
+
+	testType.false<NotAssignable<1, any>>(true)
+	testType.false<NotAssignable<null, any>>(true)
+	testType.false<NotAssignable<undefined, any>>(true)
+})
+
+it('returns true when B is `unknown` as anything can be assigned to `unknown`', () => {
+	testType.false<NotAssignable<any, unknown>>(true)
+	testType.false<NotAssignable<unknown, unknown>>(true)
+	testType.false<NotAssignable<never, unknown>>(true)
+
+	testType.false<NotAssignable<1, unknown>>(true)
+	testType.false<NotAssignable<null, unknown>>(true)
+	testType.false<NotAssignable<undefined, unknown>>(true)
+})
+
+it('returns false when B is `never` except when A is `never`', () => {
+	testType.true<NotAssignable<any, never>>(true)
+	testType.true<NotAssignable<unknown, never>>(true)
+	testType.false<NotAssignable<never, never>>(true)
+
+	testType.true<NotAssignable<1, never>>(true)
+	testType.true<NotAssignable<null, never>>(true)
+	testType.true<NotAssignable<undefined, never>>(true)
+})
+
+it('works against special types', () => {
+	testType.equal<NotAssignable<any, 1>, false>(true)
+	testType.equal<NotAssignable<unknown, 1>, false>(true)
+	testType.equal<NotAssignable<never, 1>, false>(true)
+})
+
+it('can disable distribution', () => {
+	testType.equal<NotAssignable<boolean, true>, boolean>(true)
+	testType.equal<NotAssignable<boolean, true, { distributive: false }>, true>(true)
+
+	testType.equal<NotAssignable<number | string, number>, boolean>(true)
+	testType.equal<NotAssignable<number | string, number, { distributive: false }>, true>(true)
+})
+
+it('can use as filter', () => {
+	testType.equal<NotAssignable<1, number, { selection: 'filter' }>, never>(true)
+	testType.equal<NotAssignable<number, 1, { selection: 'filter' }>, number>(true)
+})
+
+it('work as branching', () => {
+	testType.equal<NotAssignable<1, any, NotAssignable.$Branch>, $Else>(true)
+	testType.equal<NotAssignable<1, unknown, NotAssignable.$Branch>, $Else>(true)
+	testType.equal<NotAssignable<1, never, NotAssignable.$Branch>, $Then>(true)
+	testType.equal<NotAssignable<never, never, NotAssignable.$Branch>, $Else>(true)
+	testType.equal<NotAssignable<1, number, NotAssignable.$Branch>, $Else>(true)
+	testType.equal<NotAssignable<true, number, NotAssignable.$Branch>, $Then>(true)
+})
+
+it('works with partial customization', () => {
+	testType.equal<NotAssignable<any, any, { $then: 1 }>, false>(true)
+	testType.equal<NotAssignable<any, unknown, { $then: 1 }>, false>(true)
+	testType.equal<NotAssignable<never, never, { $then: 1 }>, false>(true)
+	testType.equal<NotAssignable<0, number, { $then: 1 }>, false>(true)
+	testType.equal<NotAssignable<1, never, { $else: 2 }>, true>(true)
+	testType.equal<NotAssignable<0, string, { $else: 2 }>, true>(true)
+
+	testType.equal<NotAssignable<any, any, { $else: 1 }>, 1>(true)
+	testType.equal<NotAssignable<any, unknown, { $else: 1 }>, 1>(true)
+	testType.equal<NotAssignable<never, never, { $else: 1 }>, 1>(true)
+	testType.equal<NotAssignable<0, number, { $else: 1 }>, 1>(true)
+	testType.equal<NotAssignable<1, never, { $then: 1 }>, 1>(true)
+	testType.equal<NotAssignable<0, string, { $then: 1 }>, 1>(true)
+})
+
+it('can override $any branch', () => {
+	testType.equal<NotAssignable<any, any>, false>(true)
+	testType.equal<NotAssignable<any, any, { $any: unknown }>, unknown>(true)
+	testType.equal<NotAssignable<any, number, { $any: unknown }>, unknown>(true)
+})
+
+it('can override $unknown branch', () => {
+	testType.equal<NotAssignable<unknown, unknown>, false>(true)
+	testType.equal<NotAssignable<unknown, unknown, { $unknown: unknown }>, unknown>(true)
+	testType.equal<NotAssignable<unknown, number, { $unknown: unknown }>, unknown>(true)
+})
+
+it('can override $never branch', () => {
+	testType.equal<NotAssignable<never, never>, false>(true)
+	testType.equal<NotAssignable<never, never, { $never: unknown }>, unknown>(true)
+	testType.equal<NotAssignable<never, number, { $never: unknown }>, unknown>(true)
+	testType.equal<NotAssignable<1, never, { $never: unknown }>, true>(true)
+})
---
+++ type-plus/ts/predicates/not_assignable.ts
@@ -0,0 +1,99 @@
+import type { $Any } from '../any/any.js'
+import type { $Never } from '../never/never.js'
+import type { $SpecialType } from '../type_plus/$special_type.js'
+import type { $DistributiveDefault, $DistributiveOptions } from '../type_plus/branch/$distributive.js'
+import type { $InputOptions } from '../type_plus/branch/$input_options.js'
+import type { $IsDistributive } from '../type_plus/branch/$is_distributive.js'
+import type { $ResolveBranch } from '../type_plus/branch/$resolve_branch.js'
+import type { $Else, $SelectionBranch, $SelectionPredicate, $Then } from '../type_plus/branch/$selection.js'
+import type { $SelectionOptions } from '../type_plus/branch/$selection_options.js'
+import type { $Unknown } from '../unknown/unknown.js'
+
+/**
+ * 🎭 *predicate*
+ *
+ * Validate if `A` is not assignable to `B`.
+ *
+ * @example
+ * ```ts
+ * type R = NotAssignable<any, any> // false
+ * type R = NotAssignable<any, 1> // false
+ * type R = NotAssignable<unknown, unknown> // false
+ * type R = NotAssignable<never, never> // false
+ * type R = NotAssignable<1, 1> // false
+ * type R = NotAssignable<'a', 'a'> // false
+ * type R = NotAssignable<'a', 'b'> // true
+ * type R = NotAssignable<'a', string> // false
+ * ```
+ *
+ * 🔢 *customize*
+ *
+ * Filter to ensure `A` is not assignable to `B`.
+ *
+ * @example
+ * ```ts
+ * type R = NotAssignable<any, any, { selection: 'filter' }> // never
+ * type R = NotAssignable<string, number, { selection: 'filter' }> // string
+ * ```
+ *
+ * 🔢 *customize*
+ *
+ * Use unique branch identifiers to allow precise processing of the result.
+ *
+ * @example
+ * ```ts
+ * type R = NotAssignable<any, any, NotAssignable.$Branch> // $Else
+ * ```
+ *
+ * 🔢 *customize*
+ *
+ * Override special types branch.
+ *
+ * @example
+ * ```ts
+ * type R = NotAssignable<any, any, { $any: 1 }> // 1
+ * type R = NotAssignable<unknown, any, { $unknown: 1 }> // 1
+ * type R = NotAssignable<never, any, { $never: 1 }> // 1
+ * ```
+ */
+export type NotAssignable<
+	A,
+	B,
+	$O extends NotAssignable.$Options = {}
+> = $SpecialType<B, {
+	$any: $ResolveBranch<A, $O, [0 extends 1 & A ? $Any : unknown, $Else]>,
+	$unknown: $ResolveBranch<A, $O, [[A, unknown] extends [unknown, A] ? $Unknown : unknown, $Else]>,
+	$never: $ResolveBranch<A, $O, [A, never] extends [never, A] ? [$Never, $Else] : [$Then]>,
+	$else: $SpecialType<A, {
+		$any: $ResolveBranch<A, $O, [$Any, $Else]>,
+		$unknown: $ResolveBranch<A, $O, [$Unknown, $Else]>,
+		$never: $ResolveBranch<A, $O, [$Never, $Else]>,
+		$else: NotAssignable.$<A, B, $O>
+	}>
+}>
+
+
+export namespace NotAssignable {
+	export type $Options = $SelectionOptions & $DistributiveOptions & $InputOptions<$Any | $Unknown | $Never>
+	export type $Default = $SelectionPredicate & $DistributiveDefault
+	export type $Branch<$O extends $DistributiveOptions = {}> = $SelectionBranch & $O
+
+	/**
+	 * 🧰 *type util*
+	 *
+	 * Validate if `A` is assignable to `B`.
+	 *
+	 * This is the internal logic of `NotAssignable`.
+	 * It does not check against special types.
+	 *
+	 * It is suitable for building custom types.
+	 */
+	export type $<
+		A,
+		B,
+		$O extends NotAssignable.$Options = {}
+	> = $IsDistributive<$O, {
+		$then: A extends B ? $ResolveBranch<A, $O, [$Else]> : $ResolveBranch<A, $O, [$Then]>,
+		$else: [A] extends [B] ? $ResolveBranch<A, $O, [$Else]> : $ResolveBranch<A, $O, [$Then]>,
+	}>
+}
