7ad9daffc5dbf1eda55a82047a01f042d3f2f3b0
unional <homawong@gmail.com>
2023-11-05 19:51:46 +0000

    chore: add jest to top level

--- type-plus/jest.config.mjs
+++ type-plus/jest.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('jest').Config} */
 export default {
 	preset: '@repobuddy/jest/presets/ts-esm-watch',
-	modulePathIgnorePatterns: ['<rootDir>/ts/types', '<rootDir>/ts/type-checker']
+	modulePathIgnorePatterns: ['<rootDir>/ts/types', '<rootDir>/ts/type-checker'],
+	roots: ['<rootDir>/ts'],
 }
