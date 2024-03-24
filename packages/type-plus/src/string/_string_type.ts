a5095697b8d3c73021e096350007f383458ed4d3
unional <homawong@gmail.com>
2024-03-23 17:39:19 -0700

    chore: add internal _StringType<T>

---
+++ packages/type-plus/src/string/_string_type.ts
@@ -0,0 +1,11 @@
+import type { $ExtractManipulatedString } from './$extract_manipulated_string.js'
+
+export type _StringType<T extends string> = $ExtractManipulatedString<T> extends infer K
+	? K extends string & infer U
+	? [K, U] extends [U, K]
+	? {} extends { [P in `${K}`]: unknown }
+	? 'templateLiteral'
+	: 'stringLiteral'
+	: 'string'
+	: never
+	: never
