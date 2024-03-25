b97cb8c3c4d52cc5a837e0c5f068f08b911f8a7b
unional <homawong@gmail.com>
2024-03-24 17:07:53 -0700

    test: revert overfix from lint

--- packages/type-plus/src/class/AnyConstructor.spec.ts
+++ packages/type-plus/src/class/AnyConstructor.spec.ts
@@ -3,13 +3,15 @@ import { test } from '@jest/globals'
 import type { AnyConstructor } from '../index.js'
 
 test('basic', () => {
-	const a = (() => {}) as any as AnyConstructor
+	// biome-ignore lint/complexity/useArrowFunction: on purpose
+	const a = (function () { }) as any as AnyConstructor
 
 	new a()
 })
 
 test('specify params with tuple', () => {
-	const a = (() => {}) as any as AnyConstructor<[count: number, value: string]>
+	// biome-ignore lint/complexity/useArrowFunction: on purpose
+	const a = (function () { }) as any as AnyConstructor<[count: number, value: string]>
 
 	new a(1, 'a')
 })
