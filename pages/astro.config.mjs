d4a3ed545f155ff19c2f95786474dba8723ae86b
unional <homawong@gmail.com>
2024-03-29 12:23:43 -0700

    chore: add tailwindcss to pages

--- pages/astro.config.mjs
+++ pages/astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from 'astro/config'
-
+import tailwind from '@astrojs/tailwind'
 import solidJs from '@astrojs/solid-js'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [solidJs()]
+	integrations: [solidJs(), tailwind()]
 })
