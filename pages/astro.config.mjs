605c29036c560a34c00cd5a8cb2e265e7130a355
unional <homawong@gmail.com>
2024-03-31 23:48:31 -0700

    chore: add starlight

--- pages/astro.config.mjs
+++ pages/astro.config.mjs
@@ -1,8 +1,31 @@
 import { defineConfig } from 'astro/config'
-
+import starlight from '@astrojs/starlight'
+import tailwind from '@astrojs/tailwind'
 import solidJs from '@astrojs/solid-js'
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [solidJs()]
+	integrations: [
+		tailwind(),
+		solidJs(),
+		starlight({
+			title: 'type-plus',
+			social: {
+				github: 'https://github.com/unional/type-plus'
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', link: '/guides/example/' }
+					]
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' }
+				}
+			]
+		})
+	]
 })
