c753fe8de5b9994e8443e25411639375fe6bee92
unional <homawong@gmail.com>
2024-08-02 01:12:06 -0700

    chore: update  typescript  to 5.5

--- packages/type-plus/src/array/array_plus.find.spec.ts
+++ packages/type-plus/src/array/array_plus.find.spec.ts
@@ -5,7 +5,7 @@ import { type ArrayPlus, testType } from '../index.js'
 test('behavior of array.find()', () => {
 	const array = [1, 2, '3']
 	const r = array.find((x) => typeof x === 'number')
-	testType.equal<typeof r, number | string | undefined>(true)
+	testType.equal<typeof r, number | undefined>(true)
 })
 
 it('returns never if input is never', () => {
--- packages/type-plus/src/tuple/tuple_plus.find.spec.ts
+++ packages/type-plus/src/tuple/tuple_plus.find.spec.ts
@@ -5,7 +5,7 @@ import { testType, type TuplePlus } from '../index.js'
 test('behavior of tuple.find()', () => {
 	const tuple: [1, 2, '3'] = [1, 2, '3']
 	const r = tuple.find((x) => typeof x === 'number')
-	testType.equal<typeof r, 1 | 2 | '3' | undefined>(true)
+	testType.equal<typeof r, 1 | 2 | undefined>(true)
 })
 
 // TODO: handle never and $never
