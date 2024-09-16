14ddd492c6b316ff0319c453b11372e901600765
unional <homawong@gmail.com>
2024-09-16 03:26:46 -0700

    chore: move types and type-checker to old

--- packages/type-plus/jest.config.mjs
+++ packages/type-plus/jest.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('jest').Config} */
 export default {
 	preset: '@repobuddy/jest/presets/ts-esm-watch',
-	modulePathIgnorePatterns: ['<rootDir>/src/types', '<rootDir>/src/type-checker'],
 	roots: ['<rootDir>/src'],
 }
