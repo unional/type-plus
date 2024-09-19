ef58c6fbb01b2c66ecf609f6e29afc5827a60d66
unional <homawong@gmail.com>
2024-09-19 00:48:03 -0700

    docs: add icon

--- pages/astro.config.mjs
+++ pages/astro.config.mjs
@@ -10,6 +10,11 @@ export default defineConfig({
 		solidJs(),
 		starlight({
 			title: 'type-plus',
+			logo: {
+				light: '/public/type-plus.svg',
+				dark: '/public/type-plus.svg',
+			},
+			description: 'Provides over 200 utility types and functions for applications, library, and type-level programming.',
 			social: {
 				github: 'https://github.com/unional/type-plus',
 			},
