8a1270c884886eb3a9a23b0766b23466a5b24965
unional <homawong@gmail.com>
2024-11-17 17:38:38 -0800

    docs: update doc for Widen

--- packages/type-plus/src/utils/Widen.ts
+++ packages/type-plus/src/utils/Widen.ts
@@ -1,5 +1,12 @@
 /**
- * Widen scalar types from literals to their parent types.
- * Borrow from `typical`
+ * ⚗️ *transform*
+ *
+ * Widen literal types to their respective parent types.
+ *
+ * ```ts
+ * type R = Widen<1> // number
+ * type R = Widen<true> // boolean
+ * type R = Widen<'a'> // string
+ * ```
  */
 export type Widen<T> = T extends boolean ? boolean : T extends number ? number : T extends string ? string : T
