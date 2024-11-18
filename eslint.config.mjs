7041a87b6734f91a734c5c86e65f754d2e4c1bde
unional <homawong@gmail.com>
2024-11-17 16:23:52 -0800

    chore: update linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,12 @@
-import globals from 'globals'
 import yml from 'eslint-plugin-yml'
 
 export default [
+	{
+		ignores: ['**/coverage/', '**/dist/', '**/cjs/', '**/esm/'],
+	},
 	...yml.configs['flat/standard'],
 	{
-		languageOptions: {
-			ecmaVersion: 2022,
-			sourceType: 'module',
-			globals: {
-				...globals.browser,
-				...globals.node,
-			},
-		},
-		ignores: ['.*/', 'cjs/', 'coverage/', 'dist/', 'docs/', 'esm/', 'node_modules/'],
+		ignores: ['.*/'],
 		rules: {
 			'yml/quotes': ['error', { prefer: 'single' }],
 		},
