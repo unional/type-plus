62cfb408b5110e79c6bf3fdf9ad992729d1ef855
unional <homawong@gmail.com>
2025-01-20 15:58:19 -0800

    refactor: move files under `$type`

--- packages/type-plus/src/$type/utils/no_infer.ts
+++ packages/type-plus/src/$type/utils/no_infer.ts
@@ -1,12 +1,14 @@
-import type { IsNull } from '../null/is_null.js'
-import type { Or } from '../predicates/logical.js'
-import type { $SelectionPredicate } from '../type_plus/branch/$selection.js'
-import type { IsUndefined } from '../undefined/is_undefined.js'
+import type { IsNull } from '../../null/is_null.js'
+import type { Or } from '../../predicates/logical.js'
+import type { $SelectionPredicate } from '../../type_plus/branch/$selection.js'
+import type { IsUndefined } from '../../undefined/is_undefined.js'
 
 /**
  * Prevents inference of a type parameter `T`.
  *
  * 🦴 *utilities*
+ *
+ * @deprecated TypeScript 5.4 provides a built-in `NoInfer`.
  *
  * @ajafff original version author
  * @see https://github.com/microsoft/TypeScript/issues/14829#issuecomment-298425341
