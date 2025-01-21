5857cc74778fa779a93c7198863ad2707aa1c16c
unional <unional@users.noreply.github.com>
2025-01-21 00:37:19 +0000

    deploy: d4ff111afbe2f06bff4ae02a2f6452fd6265993f

--- media/assert_type.ts
+++ media/assert_type.ts
@@ -1,5 +1,5 @@
 import { tersify } from 'tersify'
-import { isConstructor, type AnyConstructor } from '../class/index.js'
+import { type AnyConstructor, isConstructor } from '../class/index.js'
 import type { AnyFunction } from '../function/any_function.js'
 
 /**
