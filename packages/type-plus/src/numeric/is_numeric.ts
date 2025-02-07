3dcd3d8c429b8d075c2032d49c8ac58c72b018c5
unional <homawong@gmail.com>
2025-02-06 22:21:51 -0800

    refactor: use assignable.$

--- packages/type-plus/src/numeric/is_numeric.ts
+++ packages/type-plus/src/numeric/is_numeric.ts
@@ -1,4 +1,9 @@
+import type { $ResolveBranch } from '../$type/branch/$resolve_branch.js'
+import type { $Else } from '../$type/branch/$selection.js'
+import type { $Special } from '../$type/special/$special.js'
+import type { $MergeOptions } from '../$type/utils/$merge_options.js'
 import type { Equal } from '../equal/equal.js'
+import type { Assignable } from '../predicates/assignable.js'
 
 /**
  * Is `T` numeric.
@@ -11,10 +16,28 @@ import type { Equal } from '../equal/equal.js'
  * type R = IsNumeric<unknown> // false
  * ```
  */
-export type IsNumeric<T, $O extends IsNumeric.$Options = {}> = Equal<T, number | bigint, $O>
+export type IsNumeric<T, $O extends IsNumeric.$Options = {}> = $Special<
+	T,
+	$MergeOptions<
+		$O,
+		{
+			$then: $ResolveBranch<T, $O, [$Else]>
+			$else: IsNumeric.$<T, $O>
+		}
+	>
+>
 
 export namespace IsNumeric {
 	export type $Options = Equal.$Options
 	export type $Default = Equal.$Default
 	export type $Branch = Equal.$Branch
+	/**
+	 * 🧰 *type util*
+	 *
+	 * Validate if `T` is `Function`.
+	 *
+	 * This is a type util for building custom types.
+	 * It does not check against special types.
+	 */
+	export type $<T, $O extends Assignable.$UtilOptions> = Assignable.$<T, number | bigint, $O>
 }
